825dd4534dbc05411c58f9bf6eaabfe7e588a27f
Bernardo Heynemann <heynemann@gmail.com>
2011-07-02 21:36:44 -0300

    Created lib script

--- provy/__init__.py
+++ provy/__init__.py
@@ -1,3 +1,6 @@
 #!/usr/bin/python
 # -*- coding: utf-8 -*-
 
+__version__ = '0.1.0a'
+version = __version__
+Version = __version__
---
+++ setup.py
@@ -0,0 +1,45 @@
+# -*- coding: utf-8 -*-
+
+from setuptools import setup, find_packages
+from provy import __version__
+
+setup(
+    name = 'provy',
+    version = __version__,
+    description = "provy is an easy-to-use server provisioning tool.",
+    long_description = "provy is an easy-to-use server provisioning tool.",
+    keywords = 'provisioning devops infrastructure server',
+    author = 'Bernardo Heynemann',
+    author_email = 'heynemann@gmail.com',
+    url = 'http://heynemann.github.com/provy',
+    license = 'MIT',
+    classifiers = ['Development Status :: 3 - Alpha',
+                   'Intended Audience :: Developers',
+                   'License :: OSI Approved :: MIT License',
+                   'Natural Language :: English',
+                   'Operating System :: MacOS',
+                   'Operating System :: Microsoft :: Windows',
+                   'Operating System :: POSIX :: Linux',
+                   'Programming Language :: Python :: 2.6',
+                   'Topic :: System :: Installation/Setup'
+    ],
+    packages = find_packages(),
+    include_package_data = True,
+    package_data = {
+        '': ['*.template'],
+    },
+
+    install_requires=[
+        "fabric",
+        "jinja2",
+        "M2Crypto"
+    ],
+
+    entry_points = {
+        'console_scripts': [
+            'provy = provy.console:main',
+        ],
+    },
+
+)
+
