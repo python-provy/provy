242a393c924f22df9d101fba20ec2d3a11f50c10
Bernardo Heynemann <heynemann@gmail.com>
2011-07-07 11:25:30 -0300

    Docs

--- provy/core/roles.py
+++ provy/core/roles.py
@@ -49,14 +49,44 @@ Sample usage:
         self.context = context
 
     def register_template_loader(self, package_name):
+        '''
+Register the <<package_name>> module as a valid source for templates in Jinja2.
+Jinja2 will look inside a folder called 'templates' in the specified module.
+It is paramount that this module can be imported by python. The path must be well-known or be a sub-path of the provyfile.py directory.
+Sample usage:
+<pre class="sh_python">
+    class MySampleRole(Role):
+        def provision(self):
+            self.register_template_loader('my.full.namespace')
+</pre>
+        '''
         if package_name not in self.context['registered_loaders']:
             self.context['loader'].loaders.append(PackageLoader(package_name))
             self.context['registered_loaders'].append(package_name)
 
     def log(self, msg):
+        '''
+Logs a message to the console with the hour prepended.
+Sample usage:
+<pre class="sh_python">
+    class MySampleRole(Role):
+        def provision(self):
+            self.log('Hello World')
+</pre>
+        '''
         print '[%s] %s' % (datetime.now().strftime('%H:%M:%S'), msg)
 
     def schedule_cleanup(self):
+        '''
+Makes sure that this role will be cleaned up properly after the server has been provisioned. Call this method in your provision method if you need your role's cleanup method to be called.
+Warning: If you are using the proper ways of calling roles (provision_role, using) in your role, you do not need to call this method.
+Sample usage:
+<pre class="sh_python">
+    class MySampleRole(Role):
+        def provision(self):
+            self.schedule_cleanup()
+</pre>
+        '''
         has_role = False
         for role in self.context['cleanup']:
             if role.__class__ == self.__class__:
@@ -66,17 +96,60 @@ Sample usage:
             self.context['cleanup'].append(self)
 
     def provision_role(self, role):
+        '''
+Provisions a role inside your role. This method is the way to call other roles if you don't need to call any methods other than provision.
+provision_role keeps the context and lifecycle for the current server when calling the role and makes sure it is disposed correctly.
+Sample usage:
+<pre class="sh_python">
+    class MySampleRole(Role):
+        def provision(self):
+            self.provision_role(SomeOtherRole)
+</pre>
+        '''
         instance = role(self.prov, self.context)
         instance.provision()
-        self.schedule_cleanup()
+        instance.schedule_cleanup()
 
     def provision(self):
+        '''
+Base provision method. This is meant to be overriden and does not do anything.
+The provision method of each Role is what provy calls on to provision servers.
+Sample usage:
+<pre class="sh_python">
+    class MySampleRole(Role):
+        def provision(self):
+            pass
+</pre>
+        '''
         pass
 
     def cleanup(self):
+        '''
+Base cleanup method. This is meant to be overriden and does not do anything.
+The cleanup method is the method that provy calls after all Roles have been provisioned and is meant to allow Roles to perform any cleaning of resources or finish any pending operations.
+Sample usage:
+<pre class="sh_python">
+    class MySampleRole(Role):
+        def cleanup(self):
+            pass
+</pre>
+        '''
         pass
 
     def execute(self, command, stdout=True, sudo=False):
+        '''
+This method is the bread and butter of provy and is a base for most other methods that interact with remote servers.
+It allows you to perform any shell action in the remote server. It is an abstraction over fabric's run and sudo methods.
+Parameters:
+stdout - Defaults to True. If you specify this argument as False, the standard output of the command execution will not be displayed in the console.
+sudo - Defaults to False. Specifies whether this command needs to be run as the super-user.
+Sample Usage:
+<pre class="sh_python">
+    class MySampleRole(Role):
+        def provision(self):
+            self.execute('ls /', stdout=False, sudo=True)
+</pre>
+        '''
         func = sudo and fab_sudo or run
         if stdout:
             return func(command)
@@ -87,32 +160,112 @@ Sample usage:
             return func(command)
 
     def execute_python(self, command, stdout=True, sudo=False):
+        '''
+Just an abstraction over execute. This method executes the python code that is passed with python -c.
+It has the same arguments as execute.
+Sample Usage:
+<pre class="sh_python">
+    class MySampleRole(Role):
+        def provision(self):
+            self.python_execute('import os; print os.curdir', stdout=False, sudo=True)
+</pre>
+        '''
         return self.execute('''python -c "%s"''' % command, stdout=stdout, sudo=sudo)
 
     def get_logged_user(self):
+        '''
+Returns the currently logged user in the remote server.
+Sample Usage:
+<pre class="sh_python">
+    class MySampleRole(Role):
+        def provision(self):
+            self.context['my-user'] = self.get_logged_user()
+</pre>
+        '''
         return self.execute_python('import os; print os.getlogin()', stdout=False)
 
     def local_exists(self, file_path):
+        '''
+Returns True if the file exists locally.
+Sample Usage:
+<pre class="sh_python">
+    class MySampleRole(Role):
+        def provision(self):
+            if self.local_exists('/tmp/my-file'):
+                # do something
+</pre>
+        '''
         return exists(file_path)
 
     def remote_exists(self, file_path):
+        '''
+Returns True if the file exists in the remote server.
+Sample Usage:
+<pre class="sh_python">
+    class MySampleRole(Role):
+        def provision(self):
+            if self.remote_exists('/tmp/my-file'):
+                # do something
+</pre>
+        '''
         return self.execute('test -f %s; echo $?' % file_path, stdout=False) == '0'
 
     def remote_exists_dir(self, file_path):
+        '''
+Returns True if the directory exists in the remote server.
+Sample Usage:
+<pre class="sh_python">
+    class MySampleRole(Role):
+        def provision(self):
+            if self.remote_exists_dir('/tmp'):
+                # do something
+</pre>
+        '''
         return self.execute('test -d %s; echo $?' % file_path, stdout=False) == '0'
 
     def local_temp_dir(self):
+        '''
+Returns the path of a temporary directory in the local machine.
+Sample Usage:
+<pre class="sh_python">
+    class MySampleRole(Role):
+        def provision(self):
+            self.context['source_dir'] = self.local_temp_dir()
+</pre>
+        '''
         return gettempdir()
 
     def remote_temp_dir(self):
+        '''
+Returns the path of a temporary directory in the remote server.
+Sample Usage:
+<pre class="sh_python">
+    class MySampleRole(Role):
+        def provision(self):
+            self.context['target_dir'] = self.remote_temp_dir()
+</pre>
+        '''
         return self.execute_python('from tempfile import gettempdir; print gettempdir()', stdout=False)
 
-    def ensure_dir(self, path, owner=None, sudo=False):
-        if not self.remote_exists_dir(path):
-            self.execute('mkdir -p %s' % path, stdout=False, sudo=sudo)
+    def ensure_dir(self, directory, owner=None, sudo=False):
+        '''
+Make sure the specified directory exists in the remote server.
+Parameters:
+directory - Directory to be created if it does not exist.
+owner - If specified, the directory will be created under this user, otherwise the currently logged user is the owner.
+sudo - If specified, the directory is created under the super-user. This is particularly useful in conjunction with the owner parameter, to create folders for the owner where only the super-user can write.
+Sample Usage:
+<pre class="sh_python">
+    class MySampleRole(Role):
+        def provision(self):
+            self.ensure_dir('/etc/my-path', owner='myuser', sudo=True)
+</pre>
+        '''
+        if not self.remote_exists_dir(directory):
+            self.execute('mkdir -p %s' % directory, stdout=False, sudo=sudo)
 
         if owner:
-            self.change_dir_owner(path, owner)
+            self.change_dir_owner(directory, owner)
 
     def change_dir_owner(self, path, owner):
         self.execute('cd %s && chown -R %s .' % (path, owner), stdout=False, sudo=True)
