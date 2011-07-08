2b803be50f22f578616f0f1d4023e158d7d6561b
Douglas Soares de Andrade <contato@douglasandrade.com>
2011-07-08 04:59:23 -0300

    Fixes text wrapping issue and some minor pep8 fixes

--- provy/more/debian/users/user.py
+++ provy/more/debian/users/user.py
@@ -1,84 +1,85 @@
-#!/usr/bin/python
+#!/usr/bin/env python
 # -*- coding: utf-8 -*-
 
 '''Roles in this namespace are meant to provide user management operations for Debian distributions.'''
 
 from provy.core import Role
 
+
 class UserRole(Role):
     '''
-This role provides many utility methods for user management operations within Debian distributions.
-<em>Sample usage</em>
-<pre class="sh_python">
-    class MySampleRole(Role):
-        def provision(self):
-            with self.using(UserRole) as role:
-                role.ensure_user('myuser', identified_by='mypass', is_admin=True)
-</pre>
+    This role provides many utility methods for user management operations within Debian distributions.
+    <em>Sample usage</em>
+    <pre class="sh_python">
+        class MySampleRole(Role):
+            def provision(self):
+                with self.using(UserRole) as role:
+                    role.ensure_user('myuser', identified_by='mypass', is_admin=True)
+    </pre>
     '''
 
     def group_exists(self, group_name):
         '''
-Returns True if the given group exist, False otherwise.
-<em>Parameters</em>
-group_name - Name of the group to verify.
-<em>Sample usage</em>
-<pre class="sh_python">
-    class MySampleRole(Role):
-        def provision(self):
-            with self.using(UserRole) as role:
-                if role.group_exists('usersgroup'):
-                    # do something
-</pre>
+        Returns True if the given group exist, False otherwise.
+        <em>Parameters</em>
+        group_name - Name of the group to verify.
+        <em>Sample usage</em>
+        <pre class="sh_python">
+            class MySampleRole(Role):
+                def provision(self):
+                    with self.using(UserRole) as role:
+                        if role.group_exists('usersgroup'):
+                            # do something
+        </pre>
         '''
         return group_name in self.execute("cat /etc/group", stdout=False)
 
     def user_exists(self, username):
         '''
-Returns True if the given user exist, False otherwise.
-<em>Parameters</em>
-username - Name of the user to verify.
-<em>Sample usage</em>
-<pre class="sh_python">
-    class MySampleRole(Role):
-        def provision(self):
-            with self.using(UserRole) as role:
-                if role.user_exists('myuser'):
-                    # do something
-</pre>
+        Returns True if the given user exist, False otherwise.
+        <em>Parameters</em>
+        username - Name of the user to verify.
+        <em>Sample usage</em>
+        <pre class="sh_python">
+            class MySampleRole(Role):
+                def provision(self):
+                    with self.using(UserRole) as role:
+                        if role.user_exists('myuser'):
+                            # do something
+        </pre>
         '''
         return username in self.execute("cat /etc/passwd", stdout=False)
 
     def user_in_group(self, username, group_name):
         '''
-Returns True if the given user is in the given group, False otherwise.
-<em>Parameters</em>
-username - Name of the user to verify.
-group_name - Name of the group to verify.
-<em>Sample usage</em>
-<pre class="sh_python">
-    class MySampleRole(Role):
-        def provision(self):
-            with self.using(UserRole) as role:
-                if role.user_in_group('myuser', 'mygroup'):
-                    # do something
-</pre>
+        Returns True if the given user is in the given group, False otherwise.
+        <em>Parameters</em>
+        username - Name of the user to verify.
+        group_name - Name of the group to verify.
+        <em>Sample usage</em>
+        <pre class="sh_python">
+            class MySampleRole(Role):
+                def provision(self):
+                    with self.using(UserRole) as role:
+                        if role.user_in_group('myuser', 'mygroup'):
+                            # do something
+        </pre>
         '''
 
         return group_name in self.execute('groups %s' % username, stdout=False)
 
     def ensure_group(self, group_name):
         '''
-Ensures that a given user group is present in the remote server.
-<em>Parameters</em>
-group_name - Name of the group to create.
-<em>Sample usage</em>
-<pre class="sh_python">
-    class MySampleRole(Role):
-        def provision(self):
-            with self.using(UserRole) as role:
-                role.ensure_group('users-group')
-</pre>
+        Ensures that a given user group is present in the remote server.
+        <em>Parameters</em>
+        group_name - Name of the group to create.
+        <em>Sample usage</em>
+        <pre class="sh_python">
+            class MySampleRole(Role):
+                def provision(self):
+                    with self.using(UserRole) as role:
+                        role.ensure_group('users-group')
+        </pre>
         '''
         if not self.group_exists(group_name):
             self.log("Group %s not found! Creating..." % group_name)
@@ -87,21 +88,21 @@ group_name - Name of the group to create.
 
     def ensure_user(self, username, identified_by=None, home_folder=None, default_script="/bin/sh", group=None, is_admin=False):
         '''
-Ensures that a given user is present in the remote server.
-<em>Parameters</em>
-username - Name of the user.
-identified_by - Password that the user will use to login to the remote server. If set to None, the user will not have a password.
-home_folder - Defaults to /home/&lt;username&gt;. Specifies the user's home folder.
-default_script - Defaults to /bin/sh. Sets the user's default script, the one that will execute commands per default when logging in.
-group - Defaults to the name of the user. Group that this user belongs to. If the group does not exist it is created prior to user creation.
-is_admin - If set to True the user is added to the 'admin' user group as well.
-<em>Sample usage</em>
-<pre class="sh_python">
-    class MySampleRole(Role):
-        def provision(self):
-            with self.using(UserRole) as role:
-                role.ensure_user('myuser', identified_by='mypass', is_admin=True)
-</pre>
+        Ensures that a given user is present in the remote server.
+        <em>Parameters</em>
+        username - Name of the user.
+        identified_by - Password that the user will use to login to the remote server. If set to None, the user will not have a password.
+        home_folder - Defaults to /home/&lt;username&gt;. Specifies the user's home folder.
+        default_script - Defaults to /bin/sh. Sets the user's default script, the one that will execute commands per default when logging in.
+        group - Defaults to the name of the user. Group that this user belongs to. If the group does not exist it is created prior to user creation.
+        is_admin - If set to True the user is added to the 'admin' user group as well.
+        <em>Sample usage</em>
+        <pre class="sh_python">
+            class MySampleRole(Role):
+                def provision(self):
+                    with self.using(UserRole) as role:
+                        role.ensure_user('myuser', identified_by='mypass', is_admin=True)
+        </pre>
         '''
 
         is_admin_command = "-G admin"
@@ -133,4 +134,3 @@ is_admin - If set to True the user is added to the 'admin' user group as well.
             self.execute('echo "%s:%s" | chpasswd' % (username, identified_by), stdout=False, sudo=True)
 
         self.context['owner'] = username
-
