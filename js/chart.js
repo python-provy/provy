3a966fbabca695aaca5a04b10ebd30d831bef06e
Bernardo Heynemann <heynemann@gmail.com>
2011-07-13 21:16:48 -0300

    Filter by OS

--- js/chart.js
+++ js/chart.js
@@ -2,12 +2,14 @@ var moduleDocTemplate = null;
 var roleDocTemplate = null;
 var methodDocTemplate = null;
 var currentDoc = null
+var osLinks = null
 
 $(function() {
     moduleDocTemplate = $('#module-template');
     roleDocTemplate = $('#role-template');
     methodDocTemplate = $('#method-template');
     currentDoc = $('#current-doc');
+    osLinks = $('.operating-systems a');
 });
 
 google.load('visualization', '1', {packages:['orgchart']});
@@ -55,78 +57,99 @@ function formatHTML(text) {
     return returnText.join('\n');
 }
 
-function drawChart(docs) {
-    var data = new google.visualization.DataTable();
-    data.addColumn('string', 'Name');
-    data.addColumn('string', 'Parent');
-    data.addColumn('string', 'ToolTip');
-
-    var rows = getRows(docs)
-    data.addRows(rows);
-    var chart = new google.visualization.OrgChart(document.getElementById('docs'));
-
-    google.visualization.events.addListener(chart, 'select', selectHandler);
-
-    function selectHandler(e) {
-        currentDoc.fadeOut('fast', function() {
-            var item = chart.getSelection()[0];
-            if (!item) {
-                return;
-            }
-
-            var selectedValue = null;
-            if (item.row != null && item.column != null) {
-                selectedValue = data.getValue(item.row, item.column);
-            } else if (item.row != null) {
-                selectedValue = data.getValue(item.row, 2);
-            } else if (item.column != null) {
-                selectedValue = data.getValue(0, item.column);
-            }
-
-            var parts = selectedValue.split('.');
-            var doc = docs;
-            for (var i=0; i<parts.length; i++) {
-                doc = doc[parts[i]];
-            }
-
-            if (doc['__methods__'] == null) {
-                var template = moduleDocTemplate.clone();
-                template.find('.name').html(doc['__name__']);
-
-                var docstring = doc['__doc__'] || 'No documentation found for ' + doc['__name__'] + '.';
-                template.find('.docs').html(formatHTML(docstring));
-            } else {
-                var template = roleDocTemplate.clone();
-                template.find('.name').html(doc['__name__'] + ' - ' + doc['__module__']);
-
-                var docstring = doc['__doc__'] || 'No documentation found for ' + doc['__name__'] + '.';
-                template.find('.docs').html(formatHTML(docstring));
-
-                var methodContainer = template.find('.methods');
-                for (var i=0; i < doc['__methods__'].length; i++) {
-                    var method = doc['__methods__'][i];
-                    var methodTemplate = methodDocTemplate.clone();
-
-                    methodTemplate.find('.name').html(method['__name__']);
-                    var docstring = method['__doc__'] || 'No documentation found for method ' + method['__name__'] + '.';
-                    methodTemplate.find('.docs').html(formatHTML(docstring));
-
-                    methodTemplate.removeAttr('id');
-                    methodTemplate.show();
-                    methodContainer.append(methodTemplate);
-                }
-            }
-
-            currentDoc.html('');
-            template.removeAttr('id');
-            template.show();
-            currentDoc.append(template);
-            sh_highlightDocument();
-            currentDoc.fadeIn('fast');
-        });
-        
+function filterDocs(docs, namespace) {
+    var result = docs;
+    var names = namespace.split('.');
+    for (var i=0; i<names.length; i++){
+        result = result[names[i]];
     }
+    return result;
+}
 
-    chart.draw(data, {allowHtml:true});
+function drawChart(docs) {
+    var filterNamespace = function(namespace) {
+        currentDoc.fadeOut('fast');
+
+        var data = new google.visualization.DataTable();
+        data.addColumn('string', 'Name');
+        data.addColumn('string', 'Parent');
+        data.addColumn('string', 'ToolTip');
+
+        var filteredDocs = namespace ? filterDocs(docs, namespace) : docs;
+        var rows = getRows(filteredDocs)
+        data.addRows(rows);
+        var chart = new google.visualization.OrgChart(document.getElementById('docs'));
+
+        var selectHandler = function(e) {
+            currentDoc.fadeOut('fast', function() {
+                var item = chart.getSelection()[0];
+                if (!item) {
+                    return;
+                }
+
+                var selectedValue = null;
+                if (item.row != null && item.column != null) {
+                    selectedValue = data.getValue(item.row, item.column);
+                } else if (item.row != null) {
+                    selectedValue = data.getValue(item.row, 2);
+                } else if (item.column != null) {
+                    selectedValue = data.getValue(0, item.column);
+                }
+
+                var parts = selectedValue.split('.');
+                var doc = docs;
+                for (var i=0; i<parts.length; i++) {
+                    doc = doc[parts[i]];
+                }
+
+                if (doc['__methods__'] == null) {
+                    var template = moduleDocTemplate.clone();
+                    template.find('.name').html(doc['__name__']);
+
+                    var docstring = doc['__doc__'] || 'No documentation found for ' + doc['__name__'] + '.';
+                    template.find('.docs').html(formatHTML(docstring));
+                } else {
+                    var template = roleDocTemplate.clone();
+                    template.find('.name').html(doc['__name__'] + ' - ' + doc['__module__']);
+
+                    var docstring = doc['__doc__'] || 'No documentation found for ' + doc['__name__'] + '.';
+                    template.find('.docs').html(formatHTML(docstring));
+
+                    var methodContainer = template.find('.methods');
+                    for (var i=0; i < doc['__methods__'].length; i++) {
+                        var method = doc['__methods__'][i];
+                        var methodTemplate = methodDocTemplate.clone();
+
+                        methodTemplate.find('.name').html(method['__name__']);
+                        var docstring = method['__doc__'] || 'No documentation found for method ' + method['__name__'] + '.';
+                        methodTemplate.find('.docs').html(formatHTML(docstring));
+
+                        methodTemplate.removeAttr('id');
+                        methodTemplate.show();
+                        methodContainer.append(methodTemplate);
+                    }
+                }
+
+                currentDoc.html('');
+                template.removeAttr('id');
+                template.show();
+                currentDoc.append(template);
+                sh_highlightDocument();
+                currentDoc.fadeIn('fast');
+            });
+        };
+
+        google.visualization.events.addListener(chart, 'select', selectHandler);
+
+        chart.draw(data, {allowHtml:true});
+    };
+
+    osLinks.click(function(ev) {
+        var link = $(this);
+        var namespace = link.attr('data-select');
+        filterNamespace(namespace);
+    });
+
+    filterNamespace('provy.core');
 }
 
