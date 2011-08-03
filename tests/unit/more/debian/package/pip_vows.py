5834068392ec1951973a0143b4d848d03e3e4d1b
Rafael Caricio <rafael.jacinto@gmail.com>
2011-08-03 01:17:05 -0300

    Refactory in test to make it generic for use to test other roles.

--- tests/unit/more/debian/package/pip_vows.py
+++ tests/unit/more/debian/package/pip_vows.py
@@ -2,33 +2,49 @@
 
 from pyvows import Vows, expect
 
+from provy.core.roles import Role
 from provy.more.debian import PipRole
 
-class PipMockedRole(PipRole):
+class MockedRole(object):
 
-    def __init__(self, *args, **kwargs):
-        super(PipMockedRole, self).__init__(*args, **kwargs)
+    class Mock(object):
+        def __init__(self, return_value):
+            self.return_value = return_value
+            self.calls = []
+
+        def __call__(self, *args, **kwargs):
+            self.calls.append({"args": args, "kwargs": kwargs })
+            return self.return_value
+
+    def __init__(self, role_class):
         self.should_be_mocked = {}
+        self.role = role_class(prov=None, context={})
 
-    def __getattribute__(self, attr_name):
-        attribute = PipRole.__getattribute__(self, attr_name)
-        if "mock_method" != attr_name and \
-            hasattr(attribute, '__call__') and \
-                attr_name in self.should_be_mocked:
+    def __getattr__(self, attr_name):
+        if attr_name in self.should_be_mocked:
             return self.should_be_mocked[attr_name]
-        else:
-            return attribute
+
+        class Wrapper(self.role.__class__):
+            def __getattribute__(s, an):
+                return self.__getattr__(an)
+
+        def wrapper(*args, **kwargs):
+            return getattr(self.role.__class__, attr_name)(Wrapper(None, {}), *args, **kwargs)
+        return wrapper
 
     def mock_method(self, method_name, return_value):
-        class Mock(object):
-            def __init__(self):
-                self.calls = []
+        self.should_be_mocked[method_name] = MockedRole.Mock(return_value)
 
-            def __call__(self, *args, **kwargs):
-                self.calls.append({"args": args, "kwargs": kwargs })
-                return return_value
+class RoleContext(Vows.Context):
 
-        self.should_be_mocked[method_name] = Mock()
+    def _get_role(self):
+        if hasattr(self, 'parent') and self.parent:
+            return self.parent._get_role()
+        else:
+            return MockedRole(self._role_class())
+
+    def _role_class(self):
+        raise NotImplementedError
 
 @Vows.assertion
 def to_have_been_called(function):
@@ -54,12 +70,117 @@ def to_have_been_called_like(function, *args):
 def not_to_have_been_called(function):
     assert len(function.calls) == 0
 
-@Vows.batch
-class TestPipRole(Vows.Context):
+@Vows.assertion
+def to_have_attr(obj, attr_name):
+    assert hasattr(obj, attr_name)
 
-    class TestEnsurePackageInstalled(Vows.Context):
+@Vows.assertion
+def to_be(obj, type_class):
+    assert type(obj) == type_class or isinstance(obj, type_class) or obj == type_class
+
+@Vows.assertion
+def not_to_be(obj, type_class):
+    assert type(obj) != type_class and not isinstance(obj, type_class) and obj != type_class
+
+@Vows.assertion
+def to_have_key(dict_obj, key):
+    assert key in dict_obj
+
+class FakeRole(Role):
+
+    def other(self, o):
+        pass
+
+    def original_method(self, param):
+        self.other(param)
+        return "yeah!"
+
+    def yes_another(self, to_return):
+        return to_return
+
+@Vows.batch
+class TestMockRole(RoleContext):
+
+    def _role_class(self):
+        return FakeRole
+
+    def topic(self):
+        return self._get_role()
+
+    def ensure_mock_method_exists(self, topic):
+        expect(topic).to_have_attr('mock_method')
+
+    def ensure_mock_method_can_be_called(self, topic):
+        expect(topic.mock_method).to_have_attr('__call__')
+
+    def ensure_attribute_should_be_mocked_available(self, topic):
+        expect(topic).to_have_attr('should_be_mocked')
+
+    def ensure_attribute_should_be_mocked_to_be_a_dict(self, topic):
+        expect(topic.should_be_mocked).to_be(dict)
+
+    class WhenICallTheMockMethod(RoleContext):
+
+        def topic(self, role):
+            role.mock_method('other', 'fine')
+            return role
+
+        def ensure_method_included_in_mocked_dict(self, role):
+            expect(role.should_be_mocked).to_have_key('other')
+
+        def ensure_method_has_been_mocked(self, role):
+            expect(role.other).to_be(MockedRole.Mock)
+
+        class WhenICallTheMockedMethod(RoleContext):
+
+            def topic(self):
+                role = self._get_role()
+                role.mock_method('other', 'fine')
+                return [role, role.other('hello')]
+
+            def ensure_method_attached_in_role_returns_the_mocked_value(self, topic):
+                expect(topic[1]).to_equal('fine')
+
+            def should_store_the_call(self, topic):
+                expect(topic[0].other).to_have_been_called()
+
+        class TheMockedMethodShouldHaveTheOriginalMethods(RoleContext):
+
+            def should_have_the_original_method(self, role):
+                expect(role).to_have_attr('original_method')
+
+            def should_return_the_original_method(self, role):
+                expect(role.original_method).not_to_be(MockedRole.Mock)
+
+            class WhenICallTheOriginalMethodThatsCallTheMockedMethodInternally(RoleContext):
+
+                def topic(self, role):
+                    return [role, role.original_method("hello")]
+
+                def should_call_the_mocked_method(self, topic):
+                    expect(topic[0].other).to_have_been_called()
+
+                def should_execute_normally_the_original_method(self, topic):
+                    expect(topic[1]).to_equal("yeah!")
+
+            class WhenICallAnotherMethod(RoleContext):
+
+                def topic(self, role):
+                    return role.yes_another("be nice!")
+
+                def should_execute_without_modification(self, topic):
+                    expect(topic).to_equal("be nice!")
+
+
+@Vows.batch
+class TestPipRole(RoleContext):
+
+    def _role_class(self):
+        return PipRole
+
+    class TestEnsurePackageInstalled(RoleContext):
         def topic(self):
-            pip_role = PipMockedRole(prov=None, context={})
+            pip_role = self._get_role()
             pip_role.mock_method("log", None)
             pip_role.mock_method("execute", None)
             return pip_role
