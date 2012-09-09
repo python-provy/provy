bb06896c117228b93294c37e3cd5a25fa86ca9a6
Diogo Baeder de Paula Pinto <diogobaeder@yahoo.com.br>
2012-09-09 01:36:45 -0300

    Finishing port to nose and mock for PostgreSQLRole tests

--- tests/unit/more/debian/database/test_postgresql.py
+++ tests/unit/more/debian/database/test_postgresql.py
@@ -1,11 +1,38 @@
 from unittest import TestCase
 
+from mock import MagicMock, patch
 from nose.tools import istest
 
 from provy.more.debian.database.postgresql import PostgreSQLRole
 
 
 class PostgreSQLRoleTest(TestCase):
+    def setUp(self):
+        self.successful_execute = MagicMock(return_value=True)
+        self.role = PostgreSQLRole(prov=None, context={})
+        self.current_execute = None
+
+    def successful_execution(self):
+        self.current_execute = self.successful_execute
+        return patch('provy.core.roles.Role.execute', self.successful_execute)
+
+    def assert_executed(self, *args, **kwargs):
+        self.current_execute.assert_called_with(*args, **kwargs)
+
     @istest
     def creates_a_user_prompting_for_password(self):
-        role = PostgreSQLRole(prov=None, context={})
+        with self.successful_execution():
+            self.assertTrue(self.role.create_user("foo"))
+            self.assert_executed("createuser -P foo", stdout=False)
+
+    @istest
+    def creates_a_user_without_prompting_for_password(self):
+        with self.successful_execution():
+            self.assertTrue(self.role.create_user("foo", ask_password=False))
+            self.assert_executed("createuser foo", stdout=False)
+
+    @istest
+    def drops_the_user(self):
+        with self.successful_execution():
+            self.assertTrue(self.role.drop_user("foo"))
+            self.assert_executed("dropuser foo", stdout=False)
