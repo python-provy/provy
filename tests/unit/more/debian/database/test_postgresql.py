eae3ef963c5cb39bad36b96bbc5184b965c6af36
Diogo Baeder de Paula Pinto <diogobaeder@yahoo.com.br>
2012-09-09 01:43:45 -0300

    Simplifying PostgreSQLRole tests

--- tests/unit/more/debian/database/test_postgresql.py
+++ tests/unit/more/debian/database/test_postgresql.py
@@ -1,3 +1,4 @@
+from contextlib import contextmanager
 from unittest import TestCase
 
 from mock import MagicMock, patch
@@ -6,33 +7,35 @@ from nose.tools import istest
 from provy.more.debian.database.postgresql import PostgreSQLRole
 
 
-class PostgreSQLRoleTest(TestCase):
+class PostgreSQLRoleTestCase(TestCase):
     def setUp(self):
         self.successful_execute = MagicMock(return_value=True)
         self.role = PostgreSQLRole(prov=None, context={})
         self.current_execute = None
 
-    def successful_execution(self):
+    @contextmanager
+    def successful_execution(self, *args, **kwargs):
         self.current_execute = self.successful_execute
-        return patch('provy.core.roles.Role.execute', self.successful_execute)
+        with patch('provy.core.roles.Role.execute', self.successful_execute):
+            yield
+            self.assert_executed(*args, **kwargs)
 
     def assert_executed(self, *args, **kwargs):
         self.current_execute.assert_called_with(*args, **kwargs)
 
+
+class PostgreSQLRoleTest(PostgreSQLRoleTestCase):
     @istest
     def creates_a_user_prompting_for_password(self):
-        with self.successful_execution():
+        with self.successful_execution("createuser -P foo", stdout=False):
             self.assertTrue(self.role.create_user("foo"))
-            self.assert_executed("createuser -P foo", stdout=False)
 
     @istest
     def creates_a_user_without_prompting_for_password(self):
-        with self.successful_execution():
+        with self.successful_execution("createuser foo", stdout=False):
             self.assertTrue(self.role.create_user("foo", ask_password=False))
-            self.assert_executed("createuser foo", stdout=False)
 
     @istest
     def drops_the_user(self):
-        with self.successful_execution():
+        with self.successful_execution("dropuser foo", stdout=False):
             self.assertTrue(self.role.drop_user("foo"))
-            self.assert_executed("dropuser foo", stdout=False)
