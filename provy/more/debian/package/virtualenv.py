3ba095f7ea9cb4c7cb7faccbf56ac82497e501ba
Diogo Baeder de Paula Pinto <diogobaeder@yahoo.com.br>
2012-09-12 04:47:09 -0300

    docstrings for VirtualenvRole

--- provy/more/debian/package/virtualenv.py
+++ provy/more/debian/package/virtualenv.py
@@ -1,3 +1,10 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+
+'''
+Roles in this namespace are meant to provide virtualenvs and run commands inside a virtualenv, for Debian distributions.
+'''
+
 from contextlib import contextmanager
 import os
 
@@ -5,6 +12,33 @@ from provy.core import Role
 
 
 class VirtualenvRole(Role):
+    '''
+    This role provides virtualenv management. It also provides virtualenvwrapper provisioning, although it's not internally used in this role.
+    When using the object as a context manager (that is, using a "with" block) it will make sure that the virtual environment is created and that the commands that run inside it run within this same virtual environment (which affects, for example, the python and pip commands).
+    <em>Context manager parameters</em>
+    env_name - name of the virtual environment to be created and to keep activated when running commands inside the context manager.
+    <em>Properties</em>
+    base_directory - directory where the virtual environment subdirectory will be put at. For example, if you set it as "/home/johndoe/my_envs", and use venv("some_env"), it will create a virtual environment at "/home/johndoe/my_envs/some_env". Defaults to $HOME/.virtualenvs .
+    <em>Sample usage</em>
+    <pre class="sh_python">
+    from provy.core import Role
+    from provy.more.debian import VirtualenvRole
+
+    class MySampleRole(Role):
+        def provision(self):
+
+            # this example uses the defaults provided
+            with self.using(PipRole) as pip, self.using(VirtualenvRole) as venv, venv('fancylib'):
+                pip.ensure_package_installed('django')
+
+            # this is when you want to set a different base virtualenv directory
+            with self.using(PipRole) as pip, self.using(VirtualenvRole) as venv:
+                venv.base_directory = '/home/johndoe/Envs'
+                with venv('fancylib2'):
+                    pip.ensure_package_installed('tornado')
+    </pre>
+    '''
+
     def __init__(self, prov, context):
         super(VirtualenvRole, self).__init__(prov, context)
         self.user = context['user']
@@ -26,6 +60,19 @@ class VirtualenvRole(Role):
             yield
 
     def provision(self):
+        '''
+        Installs virtualenv and virtualenvwrapper, and their dependencies. This method should be called upon if overriden in base classes, or virtualenv won't work properly in the remote server.
+        <em>Sample usage</em>
+        <pre class="sh_python">
+        from provy.core import Role
+        from provy.more.debian import VirtualenvRole
+
+        class MySampleRole(Role):
+            def provision(self):
+                self.provision_role(VirtualenvRole) # does not need to be called if using with block.
+        </pre>
+        '''
+
         from provy.more.debian import PipRole
 
         with self.using(PipRole) as pip:
@@ -33,6 +80,21 @@ class VirtualenvRole(Role):
             pip.ensure_package_installed('virtualenvwrapper')
 
     def create_env(self, env_name):
+        '''
+        Creates a virtual environment.
+        <em>Parameters</em>
+        env_name - name of the virtual environment to be created.
+        <em>Sample usage</em>
+        <pre class="sh_python">
+        from provy.core import Role
+        from provy.more.debian import VirtualenvRole
+
+        class MySampleRole(Role):
+            def provision(self):
+                with self.using(VirtualenvRole) as venv:
+                    env_dir = venv.create_env('fancylib') # will return the directory where the virtual environment was created
+        </pre>
+        '''
         env_dir = os.path.join(self.base_directory, env_name)
         self.execute('virtualenv %s' % env_dir)
         return env_dir
