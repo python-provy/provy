c0fc31c80cf069b43cf29591fc9e52a4d2305d51
Diogo Baeder de Paula Pinto <diogobaeder@yahoo.com.br>
2012-09-16 00:35:27 -0300

    Covering provision

--- tests/unit/more/debian/package/test_pip.py
+++ tests/unit/more/debian/package/test_pip.py
@@ -4,7 +4,7 @@ from unittest import TestCase
 from mock import MagicMock, patch, call
 from nose.tools import istest
 
-from provy.more.debian import PipRole
+from provy.more.debian import AptitudeRole, PipRole
 
 
 NOTHING = None
@@ -115,4 +115,17 @@ class PipRoleTest(TestCase):
     def installs_a_package_by_name_and_greater_equal_version_if_its_not_installed(self):
         with self.checking_that_package(is_installed=False), self.executing('pip install django>=1.2.3'):
             self.role.ensure_package_installed('django', version='>=1.2.3')
+
+    @istest
+    def installs_necessary_packages_to_provision(self):
+        mock_aptitude = MagicMock(spec=AptitudeRole)
+
+        @contextmanager
+        def fake_using(self, klass):
+            yield mock_aptitude
+
+        with patch('provy.core.roles.Role.using', fake_using), self.executing('easy_install pip'):
+            self.role.provision()
+            install_calls = mock_aptitude.ensure_package_installed.mock_calls
+            self.assertEqual(install_calls, [call('python-setuptools'), call('python-dev')])
 
