8c59c9f4a4ed7bd3cd48dee572b12db3fe08a73e
Diogo Baeder de Paula Pinto <diogobaeder@yahoo.com.br>
2012-09-21 00:46:16 -0300

    Adding usind_stub helper method for test cases to stub Role.using and yield a Role mock

--- tests/unit/more/debian/monitoring/test_supervisor.py
+++ tests/unit/more/debian/monitoring/test_supervisor.py
@@ -1,38 +1,26 @@
 from contextlib import contextmanager
-from unittest import TestCase
 
 from mock import MagicMock, patch, call, ANY
 from nose.tools import istest
 
 from provy.more.debian import AptitudeRole, PipRole, SupervisorRole
+from tests.unit.tools.helpers import ProvyTestCase
 
 
-class SupervisorRoleTest(TestCase):
+class SupervisorRoleTest(ProvyTestCase):
     def setUp(self):
         self.role = SupervisorRole(prov=None, context={})
 
     @istest
     def installs_necessary_packages_to_provision(self):
-        mock_pip = MagicMock(spec=PipRole)
-
-        @contextmanager
-        def stub_using(self, klass):
-            yield mock_pip
-
-        with patch('provy.core.roles.Role.using', stub_using), patch('provy.core.roles.Role.register_template_loader'):
+        with self.using_stub(PipRole) as mock_pip, patch('provy.core.roles.Role.register_template_loader'):
             self.role.provision()
 
             mock_pip.ensure_package_installed.assert_called_with('supervisor')
 
     @istest
     def forces_as_sudo_to_install(self):
-        mock_pip = MagicMock(spec=PipRole)
-
-        @contextmanager
-        def stub_using(self, klass):
-            yield mock_pip
-
-        with patch('provy.core.roles.Role.using', stub_using), patch('provy.core.roles.Role.register_template_loader'):
+        with self.using_stub(PipRole) as mock_pip, patch('provy.core.roles.Role.register_template_loader'):
             mock_pip.use_sudo = False
             mock_pip.user = 'johndoe'
             self.role.provision()
---
+++ tests/unit/tools/helpers.py
@@ -0,0 +1,17 @@
+from contextlib import contextmanager
+from unittest import TestCase
+
+from mock import MagicMock, patch
+
+
+class ProvyTestCase(TestCase):
+    @contextmanager
+    def using_stub(self, role):
+        mock_role = MagicMock(spec=role)
+
+        @contextmanager
+        def stub_using(self, klass):
+            yield mock_role
+
+        with patch('provy.core.roles.Role.using', stub_using):
+            yield mock_role
