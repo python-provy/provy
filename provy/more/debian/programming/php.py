b9cd48975df3f3caec0ffd82ec31074676bd5d66
Diogo Baeder de Paula Pinto <diogobaeder@yahoo.com.br>
2012-09-24 05:28:46 -0300

    Docstring improvements

--- provy/more/debian/programming/php.py
+++ provy/more/debian/programming/php.py
@@ -5,7 +5,7 @@ from provy.more.debian.package.aptitude import AptitudeRole
 class PHPRole(Role):
     '''
     This role provides PHP utilities for Debian distributions.
-    Additionally, installs php5-dev (PHP source libraries), php-pear (PHP package management).
+    Additionally, installs php5-dev (PHP source libraries), php-pear (PHP package management) and php5-fpm (FastCGI implementation for PHP which can be used with Nginx).
 
     <em>Sample usage</em>
     <pre class="sh_python">
@@ -21,6 +21,7 @@ class PHPRole(Role):
     def provision(self):
         '''
         Installs PHP 5 (probably 5.3, depending on your server) and its dependencies.
+        If your server is a Debian (non-derived) machine, it also adds the <a href="http://www.dotdeb.org/about/" title="dotdeb">dotdeb</a> repositories for PHP 5.3, so that you can use them with AptitudeRole to install what you need.
         This method should be called upon if overriden in base classes, or PHP won't work properly in the remote server.
         <em>Sample usage</em>
         <pre class="sh_python">
