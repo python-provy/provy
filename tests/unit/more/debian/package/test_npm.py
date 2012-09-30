9b91181872b1034d9857e702ad4bf2b03900e731
Diogo Baeder de Paula Pinto <diogobaeder@yahoo.com.br>
2012-09-30 02:11:21 -0300

    Finishing coverage over NPMRole

--- tests/unit/more/debian/package/test_npm.py
+++ tests/unit/more/debian/package/test_npm.py
@@ -54,3 +54,43 @@ class NPMRoleTest(ProvyTestCase):
             self.assertFalse(self.role.is_package_installed('socket.io', '0.6.17'))
 
             execute.assert_called_with("npm --global list | egrep 'socket.io@0.6.17'", stdout=False, sudo=True)
+
+    @istest
+    def installs_a_package_if_its_not_installed_yet_by_name(self):
+        with self.execute_mock() as execute, patch('provy.more.debian.NPMRole.is_package_installed') as is_package_installed:
+            is_package_installed.return_value = False
+            self.role.ensure_package_installed('socket.io')
+
+            execute.assert_called_with('npm install --global socket.io', stdout=False, sudo=True)
+
+    @istest
+    def doesnt_install_a_package_if_its_already_installed_yet_by_name(self):
+        with self.execute_mock() as execute, patch('provy.more.debian.NPMRole.is_package_installed') as is_package_installed:
+            is_package_installed.return_value = True
+            self.role.ensure_package_installed('socket.io')
+
+            self.assertFalse(execute.called)
+
+    @istest
+    def installs_a_package_if_its_not_installed_yet_by_name_and_version(self):
+        with self.execute_mock() as execute, patch('provy.more.debian.NPMRole.is_package_installed') as is_package_installed:
+            is_package_installed.return_value = False
+            self.role.ensure_package_installed('socket.io', '0.6.17')
+
+            execute.assert_called_with('npm install --global socket.io@0.6.17', stdout=False, sudo=True)
+
+    @istest
+    def installs_a_package_if_its_not_installed_yet_by_name_and_version_with_stdout(self):
+        with self.execute_mock() as execute, patch('provy.more.debian.NPMRole.is_package_installed') as is_package_installed:
+            is_package_installed.return_value = False
+            self.role.ensure_package_installed('socket.io', '0.6.17', stdout=True)
+
+            execute.assert_called_with('npm install --global socket.io@0.6.17', stdout=True, sudo=True)
+
+    @istest
+    def installs_a_package_if_its_not_installed_yet_by_name_and_version_without_sudo(self):
+        with self.execute_mock() as execute, patch('provy.more.debian.NPMRole.is_package_installed') as is_package_installed:
+            is_package_installed.return_value = False
+            self.role.ensure_package_installed('socket.io', '0.6.17', sudo=False)
+
+            execute.assert_called_with('npm install --global socket.io@0.6.17', stdout=False, sudo=False)
