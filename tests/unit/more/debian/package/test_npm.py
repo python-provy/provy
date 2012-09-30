5bdb41fc7ff8e56d7844816427f8a98a82097eb8
Diogo Baeder de Paula Pinto <diogobaeder@yahoo.com.br>
2012-09-30 02:03:16 -0300

    Covering NPM package verification

--- tests/unit/more/debian/package/test_npm.py
+++ tests/unit/more/debian/package/test_npm.py
@@ -18,3 +18,39 @@ class NPMRoleTest(ProvyTestCase):
             self.role.provision()
 
             provision_role.assert_called_with(NodeJsRole)
+
+    @istest
+    def checks_that_a_package_is_installed_by_name(self):
+        with self.execute_mock() as execute:
+            execute.return_value = 'socket.io'
+
+            self.assertTrue(self.role.is_package_installed('socket.io'))
+
+            execute.assert_called_with("npm --global list | egrep 'socket.io'", stdout=False, sudo=True)
+
+    @istest
+    def checks_that_a_package_is_not_installed_by_name(self):
+        with self.execute_mock() as execute:
+            execute.return_value = ''
+
+            self.assertFalse(self.role.is_package_installed('socket.io'))
+
+            execute.assert_called_with("npm --global list | egrep 'socket.io'", stdout=False, sudo=True)
+
+    @istest
+    def checks_that_a_package_is_installed_by_name_and_version(self):
+        with self.execute_mock() as execute:
+            execute.return_value = 'socket.io@0.6.17'
+
+            self.assertTrue(self.role.is_package_installed('socket.io', '0.6.17'))
+
+            execute.assert_called_with("npm --global list | egrep 'socket.io@0.6.17'", stdout=False, sudo=True)
+
+    @istest
+    def checks_that_a_package_is_not_installed_by_name_and_version(self):
+        with self.execute_mock() as execute:
+            execute.return_value = ''
+
+            self.assertFalse(self.role.is_package_installed('socket.io', '0.6.17'))
+
+            execute.assert_called_with("npm --global list | egrep 'socket.io@0.6.17'", stdout=False, sudo=True)
