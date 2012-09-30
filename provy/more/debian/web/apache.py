b6b8a624a58b97cc857c7985f54b938475f2bb9b
Diogo Baeder de Paula Pinto <diogobaeder@yahoo.com.br>
2012-09-30 03:51:41 -0300

    ensure_site for ApacheRole

--- provy/more/debian/web/apache.py
+++ provy/more/debian/web/apache.py
@@ -12,3 +12,11 @@ class ApacheRole(Role):
             aptitude.ensure_package_installed('libapache2-mod-%s' % mod)
 
         self.execute('a2enmod %s' % mod, sudo=True)
+
+    def ensure_site(self, name, file, options={}):
+        available = '/etc/apache2/sites-available/%s' % name
+        enabled = '/etc/apache2/sites-enabled/%s' % name
+
+        self.update_file(file, available, options=options, sudo=True)
+        self.remote_symlink(from_file=available, to_file=enabled, sudo=True)
+        self.execute('service apache2 reload', sudo=True)
