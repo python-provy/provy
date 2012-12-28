eaa83bbcd4d823f9cc89236b7d43ae7d8c2a5a84
Diogo Baeder de Paula Pinto <diogobaeder@yahoo.com.br>
2012-12-27 23:49:34 -0200

    Starting coverage for provy.core.runner

--- tests/functional/core/test_runner.py
+++ tests/functional/core/test_runner.py
@@ -20,13 +20,23 @@ from tests.functional.fixtures.provyfile import (
 
 
 class RunnerTest(ProvyTestCase):
-    @skip('Not yet ready')
     @istest
     def runs_normal_provisioning(self):
         provfile_path = os.path.join('tests', 'functional', 'fixtures', 'provyfile')
         server_name = 'test'
         password = 'some-pass'
-        extra_options = {
-            'custom': 'Option',
-        }
-        run(provfile_path, server_name, password, extra_options)
+        run(provfile_path, server_name, password, {})
+
+        self.assertIn(Role1, provisions)
+        self.assertIn(Role2, provisions)
+        self.assertIn(Role3, provisions)
+
+        self.assertIn(Role1, cleanups)
+        self.assertIn(Role2, cleanups)
+        self.assertIn(Role3, cleanups)
+
+        self.assertIn('foo', contexts[Role1])
+        self.assertIn('bar', contexts[Role2])
+        self.assertIn('baz', contexts[Role2])
+        self.assertIn('bar', contexts[Role3])
+        self.assertIn('baz', contexts[Role3])
