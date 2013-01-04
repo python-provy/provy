4d0497f36c0bdf991d57bed8fe5e88e725ed39a1
Diogo Baeder de Paula Pinto <diogobaeder@yahoo.com.br>
2013-01-04 01:42:37 -0200

    Fixing provisioning to Debian and start provisioning to Ubuntu

--- provy/more/debian/security/selinux.py
+++ provy/more/debian/security/selinux.py
@@ -3,8 +3,20 @@ from provy.more.debian.package.aptitude import AptitudeRole
 
 
 class SELinuxRole(Role):
+    LOAD_SCRIPT = 'some script'
+
     def provision(self):
         with self.using(AptitudeRole) as aptitude:
             aptitude.ensure_package_installed('selinux-basics')
             aptitude.ensure_package_installed('selinux-policy-default')
             aptitude.ensure_package_installed('selinux-utils')
+
+        self.__complete_for_distro()
+
+        self.execute('selinux-activate', stdout=False, sudo=True)
+
+    def __complete_for_distro(self):
+        distro_info = self.get_distro_info()
+        if distro_info.distributor_id.lower() == 'ubuntu':
+            self.put_file(self.LOAD_SCRIPT, '/usr/share/initramfs-tools/scripts/init-bottom/_load_selinux_policy', sudo=True)
+            self.execute('update-initramfs -u', stdout=False, sudo=True)
--- tests/unit/more/debian/security/test_selinux.py
+++ tests/unit/more/debian/security/test_selinux.py
@@ -1,3 +1,4 @@
+from mock import call
 from nose.tools import istest
 
 from provy.more.debian import AptitudeRole, SELinuxRole
@@ -8,11 +9,31 @@ class SELinuxRoleTest(ProvyTestCase):
     def setUp(self):
         self.role = SELinuxRole(prov=None, context={'cleanup': []})
 
+    def assert_packages_installed(self, aptitude):
+        aptitude.ensure_package_installed.assert_any_call('selinux-basics')
+        aptitude.ensure_package_installed.assert_any_call('selinux-policy-default')
+        aptitude.ensure_package_installed.assert_any_call('selinux-utils')
+
     @istest
-    def installs_necessary_packages_to_provision(self):
-        with self.using_stub(AptitudeRole) as aptitude, self.execute_mock():
+    def provisions_to_debian(self):
+        with self.using_stub(AptitudeRole) as aptitude, self.execute_mock() as execute, self.provisioning_to('debian'):
             self.role.provision()
 
-            aptitude.ensure_package_installed.assert_any_call('selinux-basics')
-            aptitude.ensure_package_installed.assert_any_call('selinux-policy-default')
-            aptitude.ensure_package_installed.assert_any_call('selinux-utils')
+            self.assert_packages_installed(aptitude)
+            expected_calls = [
+                call('selinux-activate', stdout=False, sudo=True),
+            ]
+            self.assertEqual(execute.mock_calls, expected_calls)
+
+    @istest
+    def provisions_to_ubuntu(self):
+        with self.using_stub(AptitudeRole) as aptitude, self.mock_role_method('put_file') as put_file, self.execute_mock() as execute, self.provisioning_to('ubuntu'):
+            self.role.provision()
+
+            self.assert_packages_installed(aptitude)
+            put_file.assert_called_with(self.role.LOAD_SCRIPT, '/usr/share/initramfs-tools/scripts/init-bottom/_load_selinux_policy', sudo=True)
+            expected_calls = [
+                call('update-initramfs -u', stdout=False, sudo=True),
+                call('selinux-activate', stdout=False, sudo=True),
+            ]
+            self.assertEqual(execute.mock_calls, expected_calls)
