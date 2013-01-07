bd9f2aec5659b318f381fc5d0f78ce55820dc4cc
Diogo Baeder de Paula Pinto <diogobaeder@yahoo.com.br>
2013-01-07 02:13:27 -0200

    Refactoring and cleanup

--- provy/more/debian/security/selinux.py
+++ provy/more/debian/security/selinux.py
@@ -3,19 +3,22 @@ from provy.more.debian.package.aptitude import AptitudeRole
 
 
 class SELinuxRole(Role):
-    LOAD_SCRIPT = 'some script'
-
     def provision(self):
         distro_info = self.get_distro_info()
-        with self.using(AptitudeRole) as aptitude:
-            if distro_info.distributor_id.lower() == 'ubuntu':
-                aptitude.ensure_package_installed('selinux')
-            else:
-                aptitude.ensure_package_installed('selinux-basics')
-                aptitude.ensure_package_installed('selinux-policy-default')
-            aptitude.ensure_package_installed('selinux-utils')
+        is_ubuntu = distro_info.distributor_id.lower() == 'ubuntu'
 
-        if distro_info.distributor_id.lower() != 'ubuntu':
+        with self.using(AptitudeRole) as aptitude:
+            self.__install_packages(is_ubuntu, aptitude)
+
+        if not is_ubuntu:
             self.execute('selinux-activate', stdout=False, sudo=True)
 
         self.log('''SELinux provisioned. Don't forget to reboot the server twice after provisioning (see http://wiki.debian.org/SELinux/Setup ).''')
+
+    def __install_packages(self, is_ubuntu, aptitude):
+        if is_ubuntu:
+            aptitude.ensure_package_installed('selinux')
+        else:
+            aptitude.ensure_package_installed('selinux-basics')
+            aptitude.ensure_package_installed('selinux-policy-default')
+        aptitude.ensure_package_installed('selinux-utils')
