6b47c46c22f2ea6bed54ccceae58c0d8f9a7035f
Fernando Rocha <fernandogrd@yahoo.com.br>
2013-01-14 00:46:59 -0300

    Improved tests of centos PostgreSQLRole
    Thanks @diogobaeder for the feedback
    
    Signed-off-by: Fernando Rocha <fernandogrd@yahoo.com.br>

--- tests/unit/more/centos/database/test_postgresql.py
+++ tests/unit/more/centos/database/test_postgresql.py
@@ -1,4 +1,4 @@
-from mock import call, patch
+from mock import call, patch, DEFAULT
 from nose.tools import istest
 
 from provy.more.centos import YumRole, PostgreSQLRole
@@ -14,39 +14,31 @@ class PostgreSQLRoleTestCase(test_postgresql.PostgreSQLRoleTestCase):
 class PostgreSQLRoleTest(PostgreSQLRoleTestCase):
     @istest
     def ensure_initialized(self):
-         with self.failed_execution('ls -A /var/lib/pgsql/data', None):
-             with self.successful_execution('service postgresql initdb', None):
-                 self.assertTrue(self.role._ensure_initialized())
+        with self.failed_execution('ls -A /var/lib/pgsql/data', None):
+            with self.successful_execution('service postgresql initdb', None):
+                self.assertTrue(self.role._ensure_initialized())
 
-         with self.successful_execution('ls -A /var/lib/pgsql/data', None):
-             self.assertTrue(self.role._ensure_initialized())
+        with self.successful_execution('ls -A /var/lib/pgsql/data', None):
+            self.assertTrue(self.role._ensure_initialized())
 
     @istest
     def verifies_db_is_initialized(self):
-         with self.successful_execution('ls -A /var/lib/pgsql/data', None):
-             self.assertTrue(self.role._is_db_initialized())
+        with self.successful_execution('ls -A /var/lib/pgsql/data', None):
+            self.assertTrue(self.role._is_db_initialized())
 
     @istest
     def verifies_db_is_not_initialized(self):
-         with self.failed_execution('ls -A /var/lib/pgsql/data', None):
-             self.assertFalse(self.role._is_db_initialized())
+        with self.failed_execution('ls -A /var/lib/pgsql/data', None):
+            self.assertFalse(self.role._is_db_initialized())
 
     @istest
-    def verifies_is_running(self):
-        with self.execution('..running..', 'service postgresql status', None):
-            self.assertTrue(self.role._is_running())
-
-    @istest
-    def verifies_is_not_running(self):
-        with self.execution('..stoped..', 'service postgresql status', None):
-            self.assertFalse(self.role._is_running())
-
-    @istest
-    def ensures_postegres_running(self):
-        with self.execution('..stoped..', 'service postgresql status', None):
+    def ensures_postegres_is_running(self):
+        with self.execution('..stopped..', 'service postgresql status', None):
             with self.successful_execution('service postgresql start', None):
                 self.assertTrue(self.role._ensure_running())
 
+    @istest
+    def ensures_postegres_is_not_running(self):
         with self.execution('..running..', 'service postgresql status', None):
             self.assertTrue(self.role._ensure_running())
 
@@ -61,10 +53,11 @@ class PostgreSQLRoleTest(PostgreSQLRoleTestCase):
             self.assertFalse(self.role._will_start_on_boot())
 
     @istest
-    @patch('provy.more.centos.postgresql.PostgreSQLRole._ensure_initialized')
-    @patch('provy.more.centos.postgresql.PostgreSQLRole._ensure_running')
-    @patch('provy.more.centos.postgresql.PostgreSQLRole._run_on_startup')
-    def installs_necessary_packages_to_provision(self, *mocked_methods):
+    @patch.multiple(
+        'provy.more.centos.postgresql.PostgreSQLRole', _run_on_startup=DEFAULT,
+        _ensure_running=DEFAULT, _ensure_initialized=DEFAULT,
+    )
+    def installs_necessary_packages_to_provision(self, **mocked_methods):
         with self.using_stub(YumRole) as mock_yum:
             self.role.provision()
             install_calls = mock_yum.ensure_package_installed.mock_calls
@@ -72,5 +65,6 @@ class PostgreSQLRoleTest(PostgreSQLRoleTestCase):
                 install_calls,
                 [call('postgresql-server'), call('postgresql-devel')],
             )
-        for mock in mocked_methods:
+
+        for mock in mocked_methods.values():
             self.assertTrue(mock.called)
