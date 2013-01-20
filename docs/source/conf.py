fa933fd29fe20624cbe72ed6bb86e6b6c3a2b71d
Diogo Baeder de Paula Pinto <diogobaeder@yahoo.com.br>
2013-01-20 05:24:37 -0200

    Fixing docs/conf.py

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -11,12 +11,13 @@
 # All configuration values have a default; values that are commented out
 # serve to show the default.
 
-import sys, os
+import os
+import sys
 
 # If extensions (or modules to document with autodoc) are in another directory,
 # add these directories to sys.path here. If the directory is relative to the
 # documentation root, use os.path.abspath to make it absolute, like shown here.
-#sys.path.insert(0, os.path.abspath('.'))
+sys.path.insert(0, os.path.abspath('..'))
 
 # -- General configuration -----------------------------------------------------
 
@@ -170,21 +171,21 @@ htmlhelp_basename = 'provydoc'
 # -- Options for LaTeX output --------------------------------------------------
 
 latex_elements = {
-# The paper size ('letterpaper' or 'a4paper').
-#'papersize': 'letterpaper',
+    # The paper size ('letterpaper' or 'a4paper').
+    #'papersize': 'letterpaper',
 
-# The font size ('10pt', '11pt' or '12pt').
-#'pointsize': '10pt',
+    # The font size ('10pt', '11pt' or '12pt').
+    #'pointsize': '10pt',
 
-# Additional stuff for the LaTeX preamble.
-#'preamble': '',
+    # Additional stuff for the LaTeX preamble.
+    #'preamble': '',
 }
 
 # Grouping the document tree into LaTeX files. List of tuples
 # (source start file, target name, title, author, documentclass [howto/manual]).
 latex_documents = [
-  ('index', 'provy.tex', u'provy Documentation',
-   u'Bernardo Heynemann', 'manual'),
+    ('index', 'provy.tex', u'provy Documentation',
+     u'Bernardo Heynemann', 'manual'),
 ]
 
 # The name of an image file (relative to this directory) to place at the top of
@@ -227,9 +228,9 @@ man_pages = [
 # (source start file, target name, title, author,
 #  dir menu entry, description, category)
 texinfo_documents = [
-  ('index', 'provy', u'provy Documentation',
-   u'Bernardo Heynemann', 'provy', 'One line description of project.',
-   'Miscellaneous'),
+    ('index', 'provy', u'provy Documentation',
+     u'Bernardo Heynemann', 'provy', 'One line description of project.',
+     'Miscellaneous'),
 ]
 
 # Documents to append as an appendix to all manuals.
