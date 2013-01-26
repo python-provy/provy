e493b44b4d1c8b49c53b4d049430ae16b58dcf68
Diogo Baeder de Paula Pinto <diogobaeder@yahoo.com.br>
2013-01-26 00:47:07 -0200

    Documentation updates

--- provy/more/debian/programming/nodejs.py
+++ provy/more/debian/programming/nodejs.py
@@ -2,7 +2,7 @@
 # -*- coding: utf-8 -*-
 
 '''
-Roles in this namespace are meant to provide Node.JS utility methods for Debian and Ubuntu distributions.
+Roles in this namespace are meant to provide `Node.js <http://nodejs.org/>`_ utility methods for Debian and Ubuntu distributions.
 '''
 
 import re
@@ -15,31 +15,36 @@ from provy.more.debian import AptitudeRole
 
 class NodeJsRole(Role):
     '''
-    This role provides Node.JS utilities for Debian and Ubuntu distributions.
-    <em>Sample usage</em>
-    <pre class="sh_python">
-    from provy.core import Role
-    from provy.more.debian import NodeJsRole
+    This role provides `Node.js <http://nodejs.org/>`_ utilities for Debian and Ubuntu distributions.
 
-    class MySampleRole(Role):
-        def provision(self):
-            self.provision_role(NodeJsRole)
-    </pre>
-    '''
+    Example:
+    ::
 
-    def provision(self):
-        '''
-        Installs Node.JS, NPM and their dependencies. This method should be called upon if overriden in base classes, or Node won't work properly in the remote server.
-        If the server is a Debian, will install via source packages, if it's Ubuntu, will install via Chris Lea's official PPA repository.
-        <em>Sample usage</em>
-        <pre class="sh_python">
         from provy.core import Role
         from provy.more.debian import NodeJsRole
 
         class MySampleRole(Role):
             def provision(self):
-                self.provision_role(NodeJsRole) # no need to call this if using with block.
-        </pre>
+                self.provision_role(NodeJsRole)
+    '''
+
+    def provision(self):
+        '''
+        Installs `Node.js <http://nodejs.org/>`_, `NPM <https://npmjs.org/>`_ and their dependencies.
+        This method should be called upon if overriden in base classes, or Node won't work properly in the remote server.
+
+        If the server is a Debian, will install via source packages, if it's Ubuntu, will install via
+        `Chris Lea's official PPA repository <https://launchpad.net/~chris-lea/+archive/node.js/>`_.
+
+        Example:
+        ::
+
+            from provy.core import Role
+            from provy.more.debian import NodeJsRole
+
+            class MySampleRole(Role):
+                def provision(self):
+                    self.provision_role(NodeJsRole) # no need to call this if using with block.
         '''
         if self.is_already_installed():
             return
@@ -57,19 +62,22 @@ class NodeJsRole(Role):
 
     def provision_to_debian(self):
         '''
-        Installs Node.JS, NPM and their dependencies via source packages.
-        It's not recommended that you use this method directly; Instead, provision this role directly and it will find out the best way to provision.
-        Also, this method doesn't check if NodeJS is already installed before provisioning it.
-        <em>Sample usage</em>
-        <pre class="sh_python">
-        from provy.core import Role
-        from provy.more.debian import NodeJsRole
+        Installs `Node.js <http://nodejs.org/>`_, `NPM <https://npmjs.org/>`_ and their dependencies via source packages.
 
-        class MySampleRole(Role):
-            def provision(self):
-                with self.using(NodeJsRole) as nodejs:
-                    nodejs.provision_to_debian()
-        </pre>
+        It's not recommended that you use this method directly; Instead, provision this role directly and it will find out the best way to provision.
+
+        Also, this method doesn't check if Node.js is already installed before provisioning it.
+
+        Example:
+        ::
+
+            from provy.core import Role
+            from provy.more.debian import NodeJsRole
+
+            class MySampleRole(Role):
+                def provision(self):
+                    with self.using(NodeJsRole) as nodejs:
+                        nodejs.provision_to_debian()
         '''
         with self.using(AptitudeRole) as aptitude:
             aptitude.ensure_package_installed('g++')
@@ -84,19 +92,23 @@ class NodeJsRole(Role):
 
     def provision_to_ubuntu(self):
         '''
-        Installs Node.JS, NPM and their dependencies via Chris Lea's official PPA repository.
-        It's not recommended that you use this method directly; Instead, provision this role directly and it will find out the best way to provision.
-        Also, this method doesn't check if NodeJS is already installed before provisioning it.
-        <em>Sample usage</em>
-        <pre class="sh_python">
-        from provy.core import Role
-        from provy.more.debian import NodeJsRole
+        Installs `Node.js <http://nodejs.org/>`_, `NPM <https://npmjs.org/>`_ and their dependencies via
+        `Chris Lea's official PPA repository <https://launchpad.net/~chris-lea/+archive/node.js/>`_.
 
-        class MySampleRole(Role):
-            def provision(self):
-                with self.using(NodeJsRole) as nodejs:
-                    nodejs.provision_to_ubuntu()
-        </pre>
+        It's not recommended that you use this method directly; Instead, provision this role directly and it will find out the best way to provision.
+
+        Also, this method doesn't check if Node.js is already installed before provisioning it.
+
+        Example:
+        ::
+
+            from provy.core import Role
+            from provy.more.debian import NodeJsRole
+
+            class MySampleRole(Role):
+                def provision(self):
+                    with self.using(NodeJsRole) as nodejs:
+                        nodejs.provision_to_ubuntu()
         '''
         with self.using(AptitudeRole) as aptitude:
             aptitude.ensure_package_installed('python-software-properties')
@@ -110,17 +122,18 @@ class NodeJsRole(Role):
 
     def is_already_installed(self):
         '''
-        Checks if NodeJS is already installed on the server.
-        <em>Sample usage</em>
-        <pre class="sh_python">
-        from provy.core import Role
-        from provy.more.debian import NodeJsRole
+        Checks if Node.js is already installed on the server.
 
-        class MySampleRole(Role):
-            def provision(self):
-                with self.using(NodeJsRole) as nodejs:
-                    nodejs.is_already_installed() # True or False
-        </pre>
+        Example:
+        ::
+
+            from provy.core import Role
+            from provy.more.debian import NodeJsRole
+
+            class MySampleRole(Role):
+                def provision(self):
+                    with self.using(NodeJsRole) as nodejs:
+                        nodejs.is_already_installed() # True or False
         '''
         with settings(warn_only=True):
             result = self.execute('node --version')
