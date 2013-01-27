26b47d65bea4290516839002b1375872a6807a6f
Diogo Baeder de Paula Pinto <diogobaeder@yahoo.com.br>
2013-01-27 03:09:19 -0200

    Documentation updates

--- provy/more/base/database/postgresql.py
+++ provy/more/base/database/postgresql.py
@@ -2,8 +2,7 @@
 # -*- coding: utf-8 -*-
 
 '''
-Roles in this namespace are meant to provide PostgreSQL database management
-utilities.
+Roles in this namespace are meant to provide `PostgreSQL <http://www.postgresql.org/>`_ database management utilities.
 '''
 
 from provy.core import Role
@@ -11,6 +10,11 @@ from provy.core import Role
 
 class BasePostgreSQLRole(Role):
     def provision(self):
+        '''
+        This method should be implemented by the concrete PostgreSQLRole classes, according to each provisioning steps for each distribution.
+
+        :raise: NotImplementedError
+        '''
         raise NotImplementedError
 
     def _execute(self, command, stdout=True):
@@ -19,19 +23,29 @@ class BasePostgreSQLRole(Role):
     def create_user(self, username, ask_password=True, is_superuser=False, can_create_databases=False, can_create_roles=False):
         '''
         Creates a user for the database.
-        <em>Parameters</em>
-        username - name of the user to be created.
-        ask_password - if False, doesn't ask for the user password now. Defaults to True, which makes the role prompt for the password. Defaults to True.
-        is_superuser - if True, creates as a superuser and ignores can_create_databases and can_create_roles arguments (as they would be implicit). Defaults to False.
-        can_create_databases - if True, gives database creation privilege to the user. Defaults to False.
-        can_create_roles - if True, gives this user privilege to create other users. Defaults to False.
-        <em>Sample usage</em>
-        <pre class="sh_python">
+
+        :param username: Name of the user to be created.
+        :type username: :class:`str`
+        :param ask_password: If :data:`False`, doesn't ask for the user password now. Defaults to :data:`True`, which makes the role prompt for the password.
+        :type ask_password: :class:`bool`
+        :param is_superuser: If :data:`True`, creates as a superuser and ignores can_create_databases and can_create_roles arguments (as they would be implicit).
+            Defaults to :data:`False`.
+        :type is_superuser: :class:`bool`
+        :param can_create_databases: If :data:`True`, gives database creation privilege to the user. Defaults to :data:`False`.
+        :type can_create_databases: :class:`bool`
+        :param can_create_roles: If :data:`True`, gives this user privilege to create other users. Defaults to :data:`False`.
+        :type can_create_roles: :class:`bool`
+
+        :return: The result output of the execution.
+        :rtype: :class:`str`
+
+        Example:
+        ::
+
             class MySampleRole(Role):
                 def provision(self):
                     with self.using(PostgreSQLRole) as role:
                         role.create_user("john", ask_password=False)
-        </pre>
         '''
         creation_args = self.__collect_user_creation_args(ask_password, is_superuser, can_create_databases, can_create_roles)
         return self._execute("createuser -%s %s" % (''.join(creation_args), username))
@@ -49,49 +63,69 @@ class BasePostgreSQLRole(Role):
     def drop_user(self, username):
         '''
         Drops a user from the database.
-        <em>Parameters</em>
-        username - name of the user to be dropped.
-        <em>Sample usage</em>
-        <pre class="sh_python">
+
+        :param username: Name of the user to be dropped.
+        :type username: :class:`str`
+
+        :return: The result output of the execution.
+        :rtype: :class:`str`
+
+        Example:
+        ::
+
             class MySampleRole(Role):
                 def provision(self):
                     with self.using(PostgreSQLRole) as role:
                         role.drop_user("john")
-        </pre>
         '''
         return self._execute("dropuser %s" % username)
 
     def user_exists(self, username):
         '''
         Checks if a user exists in the database.
-        <em>Parameters</em>
-        username - name of the user to be checked.
-        <em>Sample usage</em>
-        <pre class="sh_python">
+
+        :param username: Name of the user to be checked.
+        :type username: :class:`str`
+
+        :return: Whether the user exists or not.
+        :rtype: :class:`bool`
+
+        Example:
+        ::
+
             class MySampleRole(Role):
                 def provision(self):
                     with self.using(PostgreSQLRole) as role:
                         role.user_exists("john") # True or False
-        </pre>
         '''
         return bool(self._execute("psql -tAc \"SELECT 1 FROM pg_roles WHERE rolname='%s'\"" % username, stdout=False))
 
     def ensure_user(self, username, ask_password=True, is_superuser=False, can_create_databases=False, can_create_roles=False):
         '''
         Ensures that a user exists in the database. If it doesn't, create it.
-        <em>Parameters</em>
-        username - name of the user to be checked/created.
-        ask_password - if False, doesn't ask for the user password now. Defaults to True, which makes the role prompt for the password.
-        is_superuser - if True, creates as a superuser and ignores can_create_databases and can_create_roles arguments (as they would be implicit). Defaults to False.
-        can_create_databases - if True, gives database creation privilege to the user. Defaults to False.
-        can_create_roles - if True, gives this user privilege to create other users. Defaults to False.
-        <em>Sample usage</em>
-        <pre class="sh_python">
+
+        :param username: Name of the user to be checked/created.
+        :type username: :class:`str`
+        :param ask_password: If :data:`False`, doesn't ask for the user password now. Defaults to :data:`True`, which makes the role prompt for the password.
+        :type ask_password: :class:`bool`
+        :param is_superuser: If :data:`True`, creates as a superuser and ignores can_create_databases and can_create_roles arguments (as they would be implicit).
+            Defaults to :data:`False`.
+        :type is_superuser: :class:`bool`
+        :param can_create_databases: If :data:`True`, gives database creation privilege to the user. Defaults to :data:`False`.
+        :type can_create_databases: :class:`bool`
+        :param can_create_roles: If :data:`True`, gives this user privilege to create other users. Defaults to :data:`False`.
+        :type can_create_roles: :class:`bool`
+
+        :return: The result output of the execution.
+        :rtype: :class:`str`
+
+        Example:
+        ::
+
             class MySampleRole(Role):
                 def provision(self):
                     with self.using(PostgreSQLRole) as role:
                         role.ensure_user("john", ask_password=False)
-        </pre>
         '''
         if not self.user_exists(username):
             self.log('User "%s" does not exist yet. Creating...' % username)
@@ -101,16 +135,22 @@ class BasePostgreSQLRole(Role):
     def create_database(self, database, owner=None):
         '''
         Creates a database.
-        <em>Parameters</em>
-        database - name of the database to be created.
-        owner - the database owner. If not provided, will be the Postgres default.
-        <em>Sample usage</em>
-        <pre class="sh_python">
+
+        :param database: Name of the database to be created.
+        :type database: :class:`str`
+        :param owner: The database owner. If not provided, will be the Postgres default.
+        :type owner: :class:`str`
+
+        :return: The result output of the execution.
+        :rtype: :class:`str`
+
+        Example:
+        ::
+
             class MySampleRole(Role):
                 def provision(self):
                     with self.using(PostgreSQLRole) as role:
                         role.create_database("foo", owner="john")
-        </pre>
         '''
         owner_arg = " -O %s" % owner if owner is not None else ""
         return self._execute("createdb %s%s" % (database, owner_arg))
@@ -118,46 +158,62 @@ class BasePostgreSQLRole(Role):
     def drop_database(self, database):
         '''
         Drops a database.
-        <em>Parameters</em>
-        database - name of the database to be dropped.
-        <em>Sample usage</em>
-        <pre class="sh_python">
+
+        :param database: Name of the database to be dropped.
+        :type database: :class:`str`
+
+        :return: The result output of the execution.
+        :rtype: :class:`str`
+
+        Example:
+        ::
+
             class MySampleRole(Role):
                 def provision(self):
                     with self.using(PostgreSQLRole) as role:
                         role.drop_database("foo")
-        </pre>
         '''
         return self._execute("dropdb %s" % database)
 
     def database_exists(self, database):
         '''
         Checks if a database exists.
-        <em>Parameters</em>
-        database - name of the database to be checked.
-        <em>Sample usage</em>
-        <pre class="sh_python">
+
+        :param database: Name of the database to be checked.
+        :type database: :class:`str`
+
+        :return: Whether the database exists.
+        :rtype: :class:`bool`
+
+        Example:
+        ::
+
             class MySampleRole(Role):
                 def provision(self):
                     with self.using(PostgreSQLRole) as role:
                         role.database_exists("foo") # True or False
-        </pre>
         '''
         return bool(self._execute('psql -tAc "SELECT 1 from pg_database WHERE datname=\'%s\'"' % database, stdout=False))
 
     def ensure_database(self, database, owner=None):
         '''
         Ensures that a database exists. If it doesn't, create it.
-        <em>Parameters</em>
-        database - name of the database to be checked/created.
-        owner - the database owner. If not provided, will be the Postgres default.
-        <em>Sample usage</em>
-        <pre class="sh_python">
+
+        :param database: Name of the database to be checked/created.
+        :type database: :class:`str`
+        :param owner: The database owner. If not provided, will be the Postgres default.
+        :type owner: :class:`str`
+
+        :return: The result output of the execution.
+        :rtype: :class:`str`
+
+        Example:
+        ::
+
             class MySampleRole(Role):
                 def provision(self):
                     with self.using(PostgreSQLRole) as role:
                         role.ensure_database("foo", owner="john")
-        </pre>
         '''
         if not self.database_exists(database):
             self.log('Database "%s" does not exist yet. Creating...' % database)
