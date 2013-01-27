33e41f380caf461ba9081db90fb4f138b4936620
Jacek Bzdak <jbzdak@gmail.com>
2013-01-27 17:45:10 +0100

    Fixed pep8 error for postgresql

--- provy/more/base/database/postgresql.py
+++ provy/more/base/database/postgresql.py
@@ -1,13 +1,15 @@
 # -*- coding: utf-8 -*-
 
 '''
-Roles in this namespace are meant to provide `PostgreSQL <http://www.postgresql.org/>`_ database management utilities.
+Roles in this namespace are meant to provide
+`PostgreSQL <http://www.postgresql.org/>`_ database management utilities.
 '''
 
 import getpass
 from provy.core import Role
 
 from StringIO import StringIO
+
 
 class BasePostgreSQLRole(Role):
     '''
@@ -20,7 +22,6 @@ class BasePostgreSQLRole(Role):
     * Administrative database is named postgres
     '''
 
-
     def execute_script(self, database, script):
         """
             Executes sql snippet via psql command.
@@ -28,18 +29,20 @@ class BasePostgreSQLRole(Role):
         """
         self.execute_script_file(database, StringIO(script))
 
-
     def execute_script_file(self, database, local_script_file):
-        sql = self.create_remote_temp_file(suffix=  "sql", cleanup=True)
+        sql = self.create_remote_temp_file(suffix="sql", cleanup=True)
         self.put_file(local_script_file, sql, sudo=True, stdout=False)
         self.change_path_owner(sql, "postgres")
-        self.execute("psql -f {temp_file} {database}".format(temp_file = sql, database = database), user="postgres")
-        self.remove_file(sql, sudo = True, stdout=False)
-
+        self.execute(
+            "psql -f {temp_file} {database}".format(
+                        temp_file=sql, database=database),
+            user="postgres")
+        self.remove_file(sql, sudo=True, stdout=False)
 
     def provision(self):
         '''
-        This method should be implemented by the concrete PostgreSQLRole classes, according to each provisioning steps for each distribution.
+        This method should be implemented by the concrete PostgreSQLRole
+         classes, according to each provisioning steps for each distribution.
 
         :raise: NotImplementedError
         '''
@@ -52,7 +55,11 @@ class BasePostgreSQLRole(Role):
         for ii in range(3):
             p = getpass.getpass(
                 "Provide password for user {user}  while provisioning {host}"
-                .format(user = username, host = self.context.get('host', "unknown")))
+                .format(
+                    user=username,
+                    host=self.context.get('host', "unknown")
+                ))
+
             r = getpass.getpass("Repeat password")
             if p != r:
                 print("Passwords did not match, repeat")
@@ -62,21 +69,28 @@ class BasePostgreSQLRole(Role):
             print ("Number of tries exceeded, bailing out.")
             raise ValueError()
 
-
-    def create_user(self, username, password = False, ask_password = True, is_superuser=False, can_create_databases=False, can_create_roles=False):
+    def create_user(self, username, password=False, ask_password=True,
+                    is_superuser=False, can_create_databases=False,
+                    can_create_roles=False):
         """
         Creates a user for the database.
 
         :param username: Name of the user to be created.
         :type username: :class:`str`
-        :param ask_password: If :data:`False`, doesn't ask for the user password now. Defaults to :data:`True`, which makes the role prompt for the password.
+        :param ask_password: If :data:`False`, doesn't ask for the user password
+            now. Defaults to :data:`True`, which makes the role prompt
+            for the password.
         :type ask_password: :class:`bool`
-        :param is_superuser: If :data:`True`, creates as a superuser and ignores can_create_databases and can_create_roles arguments (as they would be implicit).
+        :param is_superuser: If :data:`True`, creates as a superuser and ignores
+         can_create_databases and can_create_roles
+         arguments (as they would be implicit).
             Defaults to :data:`False`.
         :type is_superuser: :class:`bool`
-        :param can_create_databases: If :data:`True`, gives database creation privilege to the user. Defaults to :data:`False`.
+        :param can_create_databases: If :data:`True`, gives database creation
+            privilege to the user. Defaults to :data:`False`.
         :type can_create_databases: :class:`bool`
-        :param can_create_roles: If :data:`True`, gives this user privilege to create other users. Defaults to :data:`False`.
+        :param can_create_roles: If :data:`True`, gives this user privilege to
+        create other users. Defaults to :data:`False`.
         :type can_create_roles: :class:`bool`
 
         :return: The result output of the execution.
@@ -90,7 +104,7 @@ class BasePostgreSQLRole(Role):
                     with self.using(PostgreSQLRole) as role:
                         role.create_user("john", ask_password=False)
         """
-    
+
         script_parts = []
         script_parts.append("CREATE USER")
         script_parts.append('"{}"'.format(username))
@@ -117,34 +131,21 @@ class BasePostgreSQLRole(Role):
         script_parts.append(";")
         return self.execute_script("postgres", " ".join(script_parts))
 
-
-    def __collect_user_creation_args(self, ask_password, is_superuser, can_create_databases, can_create_roles):
-        creation_args = []
-        if ask_password:
-            creation_args.append('P')
-        creation_args.append('S' if not is_superuser else 's')
-        if not is_superuser:
-            creation_args.append('D' if not can_create_databases else 'd')
-            creation_args.append('R' if not can_create_roles else 'r')
-        return creation_args
-
-    def drop_user(self, username, ignore_if_not_exists =False):
+    def drop_user(self, username, ignore_if_not_exists=False):
         '''
         Drops a user from the database.
 
         :param username: Name of the user to be dropped.
         :type username: :class:`str`
 
-        :param ignore_if_not_exists: If set to True we will ignore if there is 
-        no user with this username. 
-        
+        :param ignore_if_not_exists: If set to True we will ignore if there is
+        no user with this username.
+
         :return: The result output of the execution.
         :rtype: :class:`str`
-        
 
         Example:
         ::
-
             class MySampleRole(Role):
                 def provision(self):
                     with self.using(PostgreSQLRole) as role:
@@ -158,7 +159,7 @@ class BasePostgreSQLRole(Role):
         script.append('''"{}"'''.format(username))
         script.append(";")
 
-        self.execute_script("postgres",  " ".join(script))
+        return self.execute_script("postgres", " ".join(script))
 
     def user_exists(self, username):
         '''
@@ -178,22 +179,31 @@ class BasePostgreSQLRole(Role):
                    with self.using(PostgreSQLRole) as role:
                        role.user_exists("john") # True or False
        '''
-        return bool(self.__execute("psql -tAc \"SELECT 1 FROM pg_roles WHERE rolname='%s'\"" % username, stdout=False))
+        return bool(self._execute("psql -tAc \"SELECT 1 FROM pg_roles WHERE rolname='%s'\"" % username, stdout=False))
 
-    def ensure_user(self, username, password = None, ask_password=True, is_superuser=False, can_create_databases=False, can_create_roles=False):
+    def ensure_user(self, username, password=None, ask_password=True,
+                    is_superuser=False, can_create_databases=False,
+                    can_create_roles=False):
         '''
         Ensures that a user exists in the database. If it doesn't, create it.
 
         :param username: Name of the user to be checked/created.
         :type username: :class:`str`
-        :param ask_password: If :data:`False`, doesn't ask for the user password now. Defaults to :data:`True`, which makes the role prompt for the password.
+        :param password: Password for the created user.
+        :param ask_password: If :data:`False`, doesn't ask for the user password
+            now. Defaults to :data:`True`, which makes the role
+            prompt for the password.
         :type ask_password: :class:`bool`
-        :param is_superuser: If :data:`True`, creates as a superuser and ignores can_create_databases and can_create_roles arguments (as they would be implicit).
+        :param is_superuser: If :data:`True`, creates as a superuser and
+        ignores can_create_databases and can_create_roles arguments (as they
+            would be implicit).
             Defaults to :data:`False`.
         :type is_superuser: :class:`bool`
-        :param can_create_databases: If :data:`True`, gives database creation privilege to the user. Defaults to :data:`False`.
+        :param can_create_databases: If :data:`True`, gives database creation
+            privilege to the user. Defaults to :data:`False`.
         :type can_create_databases: :class:`bool`
-        :param can_create_roles: If :data:`True`, gives this user privilege to create other users. Defaults to :data:`False`.
+        :param can_create_roles: If :data:`True`, gives this user privilege
+            to create other users. Defaults to :data:`False`.
         :type can_create_roles: :class:`bool`
 
         :return: The result output of the execution.
@@ -209,7 +219,9 @@ class BasePostgreSQLRole(Role):
         '''
         if not self.user_exists(username):
             self.log('User "%s" does not exist yet. Creating...' % username)
-            return self.create_user(username, password, ask_password, is_superuser, can_create_databases, can_create_roles)
+            return self.create_user(username, password, ask_password,
+                                    is_superuser, can_create_databases,
+                                    can_create_roles)
         return True
 
     def create_database(self, database, owner=None):
@@ -218,7 +230,8 @@ class BasePostgreSQLRole(Role):
 
         :param database: Name of the database to be created.
         :type database: :class:`str`
-        :param owner: The database owner. If not provided, will be the Postgres default.
+        :param owner: The database owner. If not provided,
+            will be the Postgres default.
         :type owner: :class:`str`
 
         :return: The result output of the execution.
@@ -238,7 +251,7 @@ class BasePostgreSQLRole(Role):
         script.append(";")
         self.execute_script("postgres", " ".join(script))
 
-    def drop_database(self, database, ignore_if_not_exists =False):
+    def drop_database(self, database, ignore_if_not_exists=False):
         '''
         Drops a database.
 
@@ -281,7 +294,7 @@ class BasePostgreSQLRole(Role):
                     with self.using(PostgreSQLRole) as role:
                         role.database_exists("foo") # True or False
         '''
-        return bool(self.__execute('psql -tAc "SELECT 1 from pg_database WHERE datname=\'%s\'"' % database, stdout=False))
+        return bool(self._execute('psql -tAc "SELECT 1 from pg_database WHERE datname=\'%s\'"' % database, stdout=False))
 
     def ensure_database(self, database, owner=None):
         '''
@@ -289,7 +302,8 @@ class BasePostgreSQLRole(Role):
 
         :param database: Name of the database to be checked/created.
         :type database: :class:`str`
-        :param owner: The database owner. If not provided, will be the Postgres default.
+        :param owner: The database owner. If not provided, will be the
+                        Postgres default.
         :type owner: :class:`str`
 
         :return: The result output of the execution.
