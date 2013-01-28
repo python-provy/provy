cb431dac5175988f2b7c6687e02b239c13c395ea
Diogo Baeder de Paula Pinto <diogobaeder@yahoo.com.br>
2013-01-27 22:18:54 -0200

    Documentation updates

--- provy/more/centos/database/mysql.py
+++ provy/more/centos/database/mysql.py
@@ -2,7 +2,7 @@
 # -*- coding: utf-8 -*-
 
 '''
-Roles in this namespace are meant to provide MySQL database management utilities for CentOS distributions.
+Roles in this namespace are meant to provide `MySQL <http://www.mysql.com/>`_ database management utilities for CentOS distributions.
 '''
 
 import re
@@ -13,23 +13,21 @@ from provy.more.centos.package.yum import YumRole
 
 class MySQLRole(Role):
     '''
-    This role provides MySQL database management utilities for CentOS distributions.
-    This role uses two context keys: mysql_root_user and mysql_root_pass. If none are found, it uses 'root' and empty password.
-    <em>Sample usage</em>
-    <pre class="sh_python">
+    This role provides `MySQL <http://www.mysql.com/>`_ database management utilities for CentOS distributions.
+
+    This role uses two context keys: `mysql_root_user` and `mysql_root_pass`. If none are found, it uses 'root' and empty password.
+
+    Example:
+    ::
+
         from provy.core import Role
         from provy.more.centos import MySQLRole
 
         class MySampleRole(Role):
             def provision(self):
                 with self.using(MySQLRole) as role:
-                    role.ensure_user(username=self.context['mysql_user'],
-                                     identified_by=self.context['mysql_password'])
-
-                    role.ensure_database(self.context['mysql_database'],
-                                         owner=self.context['mysql_user'])
-
-    </pre>
+                    role.ensure_user(username=self.context['mysql_user'], identified_by=self.context['mysql_password'])
+                    role.ensure_database(self.context['mysql_database'], owner=self.context['mysql_user'])
     '''
     def __init__(self, prov, context):
         super(MySQLRole, self).__init__(prov, context)
@@ -38,13 +36,15 @@ class MySQLRole(Role):
 
     def provision(self):
         '''
-        Installs MySQL Server and its dependencies. This method should be called upon if overriden in base classes, or MySQL won't work properly in the remote server.
-        <em>Sample usage</em>
-        <pre class="sh_python">
+        Installs `MySQL <http://www.mysql.com/>`_ Server and its dependencies.
+        This method should be called upon if overriden in base classes, or MySQL won't work properly in the remote server.
+
+        Example:
+        ::
+
             class MySampleRole(Role):
                 def provision(self):
                     self.provision_role(MySQLRole) # no need to call this if using with block.
-        </pre>
         '''
         with self.using(YumRole) as role:
 
@@ -99,16 +99,20 @@ class MySQLRole(Role):
     def get_user_hosts(self, username):
         '''
         Returns all the available hosts that this user can login from.
-        <em>Parameters</em>
-        username - Name of the user to be verified.
-        <em>Sample usage</em>
-        <pre class="sh_python">
+
+        :param username: Name of the user to be verified.
+        :type username: :class:`str`
+        :return: The user hosts.
+        :rtype: :class:`list`
+
+        Example:
+        ::
+
             class MySampleRole(Role):
                 def provision(self):
                     with self.using(MySQLRole) as role:
                         if not '%' in role.get_user_hosts('someuser'):
-                            # do something
-        </pre>
+                            pass
         '''
         users = self.__execute_query("select Host from mysql.user where LOWER(User)='%s'" % username.lower())
         hosts = []
@@ -120,35 +124,46 @@ class MySQLRole(Role):
 
     def user_exists(self, username, login_from='%'):
         '''
-        Returns True if the given user exists for the given location in mysql server.
-        <em>Parameters</em>
-        username - Name of the user to be verified.
-        login_from - Locations that this user can login from. Defaults to '%' (anywhere).
-        <em>Sample usage</em>
-        <pre class="sh_python">
+        Returns :data:`True` if the given user exists for the given location in mysql server.
+
+        :param username: Name of the user to be verified.
+        :type username: :class:`str`
+        :param login_from: Locations that this user can login from. Defaults to '%' (anywhere).
+        :type login_from: :class:`str`
+        :return: Whether the user exists.
+        :rtype: :class:`bool`
+
+        Example:
+        ::
+
             class MySampleRole(Role):
                 def provision(self):
                     with self.using(MySQLRole) as role:
                         if not role.user_exists('someuser'):
-                            # do something
-        </pre>
+                            pass
         '''
         return login_from in self.get_user_hosts(username)
 
     def ensure_user(self, username, identified_by, login_from='%'):
         '''
         Ensure the given user is created in the database and can login from the specified location.
-        <em>Parameters</em>
-        username - Name of the user to be created.
-        identified_by - Password that the user will use to login to mysql server.
-        login_from - Locations that this user can login from. Defaults to '%' (anywhere).
-        <em>Sample usage</em>
-        <pre class="sh_python">
+
+        :param username: Name of the user to be created.
+        :type username: :class:`str`
+        :param identified_by: Password that the user will use to login to mysql server.
+        :type identified_by: :class:`str`
+        :param login_from: Locations that this user can login from. Defaults to '%' (anywhere).
+        :type login_from: :class:`str`
+        :return: Whether the user had to be created or not.
+        :rtype: :class:`bool`
+
+        Example:
+        ::
+
             class MySampleRole(Role):
                 def provision(self):
                     with self.using(MySQLRole) as role:
                         role.ensure_user('someuser', 'somepass', 'localhost')
-        </pre>
         '''
         if not self.user_exists(username, login_from):
             self.__execute_non_query("CREATE USER '%s'@'%s' IDENTIFIED BY '%s';" % (username, login_from, identified_by))
@@ -159,17 +174,21 @@ class MySQLRole(Role):
 
     def is_database_present(self, database_name):
         '''
-        Returns True if the database is already created.
-        <em>Parameters</em>
-        database_name - Database to verify.
-        <em>Sample usage</em>
-        <pre class="sh_python">
+        Returns :data:`True` if the database is already created.
+
+        :param database_name: Database to verify.
+        :type database_name: :class:`str`
+        :return: Whether the database is present or not.
+        :rtype: :class:`bool`
+
+        Example:
+        ::
+
             class MySampleRole(Role):
                 def provision(self):
                     with self.using(MySQLRole) as role:
                         if role.is_database_present('database'):
-                            # do something
-        </pre>
+                            pass
         '''
         result = self.__execute_query('SHOW DATABASES')
         is_there = False
@@ -183,15 +202,19 @@ class MySQLRole(Role):
     def ensure_database(self, database_name):
         '''
         Creates the database if it does not exist.
-        <em>Parameters</em>
-        database_name - Database to create.
-        <em>Sample usage</em>
-        <pre class="sh_python">
+
+        :param database_name: Database to create.
+        :type database_name: :class:`str`
+        :return: Whether the database had to be created or not.
+        :rtype: :class:`bool`
+
+        Example:
+        ::
+
             class MySampleRole(Role):
                 def provision(self):
                     with self.using(MySQLRole) as role:
                         role.ensure_database('database')
-        </pre>
         '''
         if not self.is_database_present(database_name):
             self.__execute_non_query('CREATE DATABASE %s' % database_name)
@@ -202,17 +225,22 @@ class MySQLRole(Role):
     def get_user_grants(self, username, login_from='%'):
         '''
         Returns all grants for the given user at the given location.
-        <em>Parameters</em>
-        username - User to verify.
-        login_from - Locations that this user can login from. Defaults to '%' (anywhere).
-        <em>Sample usage</em>
-        <pre class="sh_python">
+
+        :param username: Name of the user to be verify.
+        :type username: :class:`str`
+        :param login_from: Locations that this user can login from. Defaults to '%' (anywhere).
+        :type login_from: :class:`str`
+        :return: The user grants.
+        :rtype: :class:`list`
+
+        Example:
+        ::
+
             class MySampleRole(Role):
                 def provision(self):
                     with self.using(MySQLRole) as role:
                         if role.get_user_grants('user', login_from='%'):
-                            # do something
-        </pre>
+                            pass
         '''
         return_grants = []
         grants = self.__execute_query("SHOW GRANTS FOR '%s'@'%s';" % (username, login_from))
@@ -223,15 +251,24 @@ class MySQLRole(Role):
 
     def has_grant(self, privileges, on, username, login_from, with_grant_option):
         '''
-        Returns True if the user has the specified privileges on the specified object in the given location.
-        <em>Parameters</em>
-        privileges - Privileges that are being verified.
-        on - Database object that the user holds privileges on.
-        username - User to verify.
-        login_from - Locations that this user can login from.
-        with_grant_option - Indicates if we are verifying against grant option.
-        <em>Sample usage</em>
-        <pre class="sh_python">
+        Returns :data:`True` if the user has the specified privileges on the specified object in the given location.
+
+        :param privileges: Privileges that are being verified.
+        :type privileges: :class:`str`
+        :param on: Database object that the user holds privileges on.
+        :type on: :class:`str`
+        :param username: Name of the user to be verify.
+        :type username: :class:`str`
+        :param login_from: Locations that this user can login from. Defaults to '%' (anywhere).
+        :type login_from: :class:`str`
+        :param with_grant_option: Indicates if we are verifying against grant option.
+        :type with_grant_option: :class:`bool`
+        :return: Whether the user has the privileges or not.
+        :rtype: :class:`bool`
+
+        Example:
+        ::
+
             class MySampleRole(Role):
                 def provision(self):
                     with self.using(MySQLRole) as role:
@@ -240,8 +277,7 @@ class MySQLRole(Role):
                                           'user',
                                           login_from='%',
                                           with_grant_option=True):
-                            # do something
-        </pre>
+                            pass
         '''
 
         grants = self.get_user_grants(username, login_from)
@@ -262,23 +298,31 @@ class MySQLRole(Role):
     def ensure_grant(self, privileges, on, username, login_from="%", with_grant_option=False):
         '''
         Ensures that the given user has the given privileges on the specified location.
-        <em>Parameters</em>
-        privileges - Privileges to assign to user (i.e.: "ALL PRIVILEGES").
-        on - Object to assign privileges to. If only the name is supplied, '.*' will be appended to the name. If you want all databases pass '*.*'.
-        username - User to grant the privileges to.
-        login_from - Location where the user gets the grants. Defaults to '%'.
-        with_grant_option - If True, indicates that this user may grant other users the same privileges. Defaults to False.
-        <em>Sample usage</em>
-        <pre class="sh_python">
+
+        :param privileges: Privileges to assign to user (e.g.: "ALL PRIVILEGES").
+        :type privileges: :class:`str`
+        :param on: Object to assign privileges to. If only the name is supplied, '.*' will be appended to the name. If you want all databases pass '*.*'.
+        :type on: :class:`str`
+        :param username: User to grant the privileges to.
+        :type username: :class:`str`
+        :param login_from: Location where the user gets the grants. Defaults to '%' (anywhere).
+        :type login_from: :class:`str`
+        :param with_grant_option: If :data:`True`, indicates that this user may grant other users the same privileges. Defaults to :data:`False`.
+        :type with_grant_option: :class:`bool`
+        :return: Whether the grant had to be added or not.
+        :rtype: :class:`bool`
+
+        Example:
+        ::
+
             class MySampleRole(Role):
                 def provision(self):
                     with self.using(MySQLRole) as role:
                         role.ensure_grant('ALL PRIVILEGES',
                                           on='database',
                                           username='backend',
-                                          login_from'%',
+                                          login_from='%',
                                           with_grant_option=True)
-        </pre>
         '''
 
         if self.has_grant(privileges, on, username, login_from, with_grant_option):
