5f3e673784c020a8296bc813c72d786306264810
Diogo Baeder de Paula Pinto <diogobaeder@yahoo.com.br>
2013-02-13 22:16:56 -0200

    Preparing for 0.6.1

--- provy/__init__.py
+++ provy/__init__.py
@@ -6,7 +6,7 @@ This is provy's main namespace. All built-in roles start from this namespace.
 '''
 
 major_version = '0.6'
-release = '1-dev'
+release = '1'
 
 __version__ = '%s.%s' % (major_version, release)
 version = __version__
