c68fbf164b5ad4a1078501463a4e2815369e2276
Diogo Baeder de Paula Pinto <diogobaeder@yahoo.com.br>
2013-06-02 19:24:07 -0300

    Covering NginxRole.ensure_site_disabled and ensure_site_enabled

--- tests/unit/more/debian/web/test_nginx.py
+++ tests/unit/more/debian/web/test_nginx.py
@@ -59,3 +59,49 @@ class NginxRoleTest(ProvyTestCase):
             self.role.ensure_conf(conf_template, options, nginx_conf_path)
 
             self.assertFalse(self.role.ensure_restart.called)
+
+    @istest
+    def ensures_site_is_disabled_and_restarted(self):
+        site = 'some-site'
+
+        with self.mock_role_methods('remove_file', 'ensure_restart'):
+            self.role.remove_file.return_value = True
+
+            self.role.ensure_site_disabled(site)
+
+            self.role.remove_file.assert_called_once_with('/etc/nginx/sites-enabled/some-site', sudo=True)
+            self.role.ensure_restart.assert_called_once_with()
+
+    @istest
+    def doesnt_restart_if_site_wasnt_disabled(self):
+        site = 'some-site'
+
+        with self.mock_role_methods('remove_file', 'ensure_restart'):
+            self.role.remove_file.return_value = False
+
+            self.role.ensure_site_disabled(site)
+
+            self.assertFalse(self.role.ensure_restart.called)
+
+    @istest
+    def ensures_site_is_enabled_and_restarted(self):
+        site = 'some-site'
+
+        with self.mock_role_methods('remote_symlink', 'ensure_restart'):
+            self.role.remote_symlink.return_value = True
+
+            self.role.ensure_site_enabled(site)
+
+            self.role.remote_symlink.assert_called_once_with('/etc/nginx/sites-available/some-site', '/etc/nginx/sites-enabled/some-site', sudo=True)
+            self.role.ensure_restart.assert_called_once_with()
+
+    @istest
+    def doesnt_restart_if_site_wasnt_enabled(self):
+        site = 'some-site'
+
+        with self.mock_role_methods('remote_symlink', 'ensure_restart'):
+            self.role.remote_symlink.return_value = False
+
+            self.role.ensure_site_enabled(site)
+
+            self.assertFalse(self.role.ensure_restart.called)
