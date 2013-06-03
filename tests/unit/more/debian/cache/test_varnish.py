6e0a1cb2cb7c7844c1b3d32a287e1a67a861044e
Diogo Baeder de Paula Pinto <diogobaeder@yahoo.com.br>
2013-06-02 23:11:37 -0300

    VarnishRole.provision_to_debian

--- tests/unit/more/debian/cache/test_varnish.py
+++ tests/unit/more/debian/cache/test_varnish.py
@@ -18,3 +18,13 @@ class VarnishRoleTest(ProvyTestCase):
             self.assertEqual(aptitude.ensure_package_installed.mock_calls, [
                 call('varnish'),
             ])
+
+    @istest
+    def installs_necessary_packages_to_provision_to_debian(self):
+        with self.using_stub(AptitudeRole) as aptitude:
+            self.role.provision_to_debian()
+
+            aptitude.ensure_gpg_key.assert_called_with('http://repo.varnish-cache.org/debian/GPG-key.txt')
+            aptitude.ensure_aptitude_source.assert_called_with('deb http://repo.varnish-cache.org/debian/ wheezy varnish-3.0')
+            aptitude.force_update.assert_called_with()
+            aptitude.ensure_package_installed.assert_called_with('varnish')
