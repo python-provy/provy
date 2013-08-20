8cba0570d06ce1916ad0c7b30a3cb465eb3bf8d0
Diogo Baeder de Paula Pinto <diogobaeder@yahoo.com.br>
2013-08-20 04:28:32 -0300

    Increasing branch coverage for provy.core.roles; refactoring

--- provy/core/runner.py
+++ provy/core/runner.py
@@ -21,6 +21,67 @@ def run(provfile_path, server_name, password, extra_options):
     prov = import_module(module_name)
     servers = get_servers_for(prov, server_name)
 
+    build_prompt_options(servers, extra_options)
+
+    for server in servers:
+        provision_server(server, provfile_path, password, prov)
+
+
+def print_header(msg):
+    print
+    print "*" * len(msg)
+    print msg
+    print "*" * len(msg)
+
+
+def provision_server(server, provfile_path, password, prov):
+    host_string = "%s@%s" % (server['user'], server['address'].strip())
+
+    context = {
+        'abspath': dirname(abspath(provfile_path)),
+        'path': dirname(provfile_path),
+        'owner': server['user'],
+        'cleanup': [],
+        'registered_loaders': []
+    }
+
+    if 'options' in server and server['options']:
+        for key, value in server['options'].iteritems():
+            context[key] = value
+
+    loader = ChoiceLoader([
+        FileSystemLoader(join(context['abspath'], 'files'))
+    ])
+    context['loader'] = loader
+
+    print_header("Provisioning %s..." % host_string)
+
+    settings_dict = dict(host_string=host_string, password=password)
+    if 'ssh_key' in server and server['ssh_key']:
+        settings_dict['key_filename'] = server['ssh_key']
+
+    with _settings(**settings_dict):
+        context['host'] = server['address']
+        context['user'] = server['user']
+        role_instances = []
+
+        try:
+            for role in server['roles']:
+                context['role'] = role
+                instance = role(prov, context)
+                role_instances.append(instance)
+                instance.provision()
+        finally:
+            for role in role_instances:
+                role.cleanup()
+
+            for role in context['cleanup']:
+                role.cleanup()
+
+    print_header("%s provisioned!" % host_string)
+
+
+def build_prompt_options(servers, extra_options):
     for server in servers:
         if 'options' in server:
             for option_name, option in server['options'].iteritems():
@@ -30,61 +91,6 @@ def run(provfile_path, server_name, password, extra_options):
                     else:
                         value = option.get_value(server)
                     server['options'][option_name] = value
-
-    for server in servers:
-        host_string = "%s@%s" % (server['user'], server['address'].strip())
-
-        context = {
-            'abspath': dirname(abspath(provfile_path)),
-            'path': dirname(provfile_path),
-            'owner': server['user'],
-            'cleanup': [],
-            'registered_loaders': []
-        }
-
-        if 'options' in server and server['options']:
-            for key, value in server['options'].iteritems():
-                context[key] = value
-
-        loader = ChoiceLoader([
-            FileSystemLoader(join(context['abspath'], 'files'))
-        ])
-        context['loader'] = loader
-
-        msg = "Provisioning %s..." % host_string
-        print
-        print "*" * len(msg)
-        print msg
-        print "*" * len(msg)
-
-        settings_dict = dict(host_string=host_string, password=password)
-        if 'ssh_key' in server and server['ssh_key']:
-            settings_dict['key_filename'] = server['ssh_key']
-
-        with _settings(**settings_dict):
-            context['host'] = server['address']
-            context['user'] = server['user']
-            role_instances = []
-
-            try:
-                for role in server['roles']:
-                    context['role'] = role
-                    instance = role(prov, context)
-                    role_instances.append(instance)
-                    instance.provision()
-            finally:
-                for role in role_instances:
-                    role.cleanup()
-
-                for role in context['cleanup']:
-                    role.cleanup()
-
-        msg = "%s provisioned!" % host_string
-        print
-        print "*" * len(msg)
-        print msg
-        print "*" * len(msg)
-        print
 
 
 def get_servers_for(prov, server_name):
