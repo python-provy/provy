214dc47d09a341f270605ab5a654f5a23a68a1b3
Diogo Baeder de Paula Pinto <diogobaeder@yahoo.com.br>
2013-09-09 22:30:07 -0300

    Basic files to provision for e2e tests

---
+++ tests/end_to_end/files/website.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+
+import sys
+import tornado.ioloop
+import tornado.web
+
+class MainHandler(tornado.web.RequestHandler):
+    def get(self):
+        self.write("Hello, world")
+
+application = tornado.web.Application([
+    (r"/", MainHandler),
+])
+
+if __name__ == "__main__":
+    port = int(sys.argv[1])
+    application.listen(port, '0.0.0.0')
+    print ">> Website running at http://0.0.0.0:%d" % port
+    tornado.ioloop.IOLoop.instance().start()
