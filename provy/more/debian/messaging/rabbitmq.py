e3d3709d4548e9199b76da00b8569cb780b0c0c5
Geovani de Souza <geovanisouza92@gmail.com>
2013-12-22 16:08:42 -0200

    Adding support for RabbitMQ on Debian

---
+++ provy/more/debian/messaging/rabbitmq.py
@@ -0,0 +1,258 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+
+'''
+Roles in this namespace are meant to provide `RabbitMQ <http://www.rabbitmq.com/>`_ utilities methods within Debian distributions.
+'''
+
+from provy.core import Role
+from provy.more.debian.package.aptitude import AptitudeRole
+
+from fabric.utils import warn
+
+
+GUEST_USER_WARNING = ('It is advisable to delete the guest user or change the'
+                      ' password to something private, particularly if your broker'
+                      ' is accessible publicly.')
+
+
+class RabbitMqRole(Role):
+    '''
+    This role provides utility methods for `RabbitMQ <http://www.rabbitmq.com/>`_ utilities within Debian distributions.
+
+    Example:
+    ::
+
+        from provy.core import Role
+        from provy.more.debian import RabbitMqRole
+        from provy.more.debian import HostNameRole
+
+        class MySampleRole(Role):
+            def provision(self):
+
+                with self.using(HostNameRole) as role:
+                    # From rabbitmq docs [1]:
+                    # "RabbitMQ names the database directory using the current
+                    # hostname of the system. If the hostname changes, a new empty
+                    # database is created.  To avoid data loss it's crucial to set
+                    # up a fixed and resolvable hostname"
+                    #
+                    # [1] http://www.rabbitmq.com/ec2.html
+
+                    role.ensure_hostname('rabbit')
+
+                with self.using(RabbitMqRole) as role:
+                    role.delete_user('guest')
+                    role.ensure_user(
+                        self.context['rabbit_user'],
+                        self.context['rabbit_password'],
+                    )
+                    role.ensure_vhost(self.context['rabbit_vhost'])
+                    role.ensure_permission(
+                        self.context['rabbit_vhost'],
+                        self.context['rabbit_user'],
+                        '".*" ".*" ".*"',
+                    )
+    '''
+    def provision(self):
+        '''
+        Installs `RabbitMQ <http://www.rabbitmq.com/>`_ and dependencies.
+        This method should be called upon if overriden in base classes, or RabbitMQ won't work properly in the remote server.
+
+        Example:
+        ::
+
+            from provy.core import Role
+            from provy.more.debian import HgRole
+
+            class MySampleRole(Role):
+                def provision(self):
+                    self.provision_role(RabbitMqRole)
+        '''
+        with self.using(AptitudeRole) as role:
+            role.ensure_up_to_date()
+            role.ensure_package_installed('rabbitmq-server')
+
+        # Start rabbitmq at startup, TODO: add chkconfig role
+        self.execute('update-rc.d rabbitmq-server defaults', stdout=False,
+                     sudo=True)
+        self.execute('update-rc.d rabbitmq-server enable', stdout=False, sudo=True)
+
+        # Make sure rabbit is running:
+        if not self.is_process_running('rabbitmq-server'):
+            self.execute(
+                'service rabbitmq-server start', stdout=False, sudo=True,
+            )
+
+        if self.user_exists('guest'):
+            warn(GUEST_USER_WARNING)
+
+    def user_exists(self, username):
+        '''
+        Checks if the RabbitMQ user exists.
+
+        :param username: Name of the user to be checked.
+        :type username: :class:`str`
+
+        :return: Whether the user exists or not.
+        :rtype: :class:`bool`
+
+        Example:
+        ::
+
+            class MySampleRole(Role):
+                def provision(self):
+                    with self.using(RabbitMqRole) as role:
+                        role.user_exists('johndoe')
+        '''
+        cmd = 'rabbitmqctl list_users'
+        users = self.execute(cmd, stdout=False, sudo=True)
+        return username in users
+
+    def vhost_exists(self, vhost):
+        '''
+        Checks if the RabbitMQ vhost exists.
+
+        :param vhost: Name of the vhost to be checked.
+        :type vhost: :class:`str`
+
+        :return: Whether the vhost exists or not.
+        :rtype: :class:`bool`
+
+        Example:
+        ::
+
+            class MySampleRole(Role):
+                def provision(self):
+                    with self.using(RabbitMqRole) as role:
+                        role.vhost_exists('foobarhost')
+        '''
+        vhs = self.execute('rabbitmqctl list_vhosts', stdout=False, sudo=True)
+        vhs = vhs.split('\r\n')[1:-1]
+        return vhost in vhs
+
+    def ensure_user(self, username, password):
+        '''
+        Ensure the given user is created in the database and can authenticate with RabbitMQ.
+
+        :param username: Name of the user to be created.
+        :type username: :class:`str`
+        :param password: Password that the user will use to authenticate to RabbitMQ.
+        :type password: :class:`str`
+
+        :return: Whether the user had to be created or not.
+        :rtype: :class:`bool`
+
+        Example:
+        ::
+
+            class MySampleRole(Role):
+                def provision(self):
+                    with self.using(RabbitMqRole) as role:
+                        role.ensure_user(some_user, some_pass)
+        '''
+        if not self.user_exists(username):
+            self.log('Setting up user %s and password' % username)
+
+            cmd = 'rabbitmqctl add_user %s %s' % (username, password)
+            self.execute(cmd, sudo=True)
+
+            self.log('User %s added!' % username)
+            return True
+
+        return False
+
+    def delete_user(self, user):
+        '''
+        Delete user from rabbitmq if exists
+
+        :param user: Name of the user to be deleted.
+        :type user: :class:`str`
+
+        Example:
+        ::
+
+            class MySampleRole(Role):
+                def provision(self):
+                    with self.using(RabbitMqRole) as role:
+                        role.delete_user('guest', some_pass)
+        '''
+        if self.user_exists(user):
+            self.log('User %s exists, deleting')
+
+            cmd = 'rabbitmqctl delete_user %s' % user
+            self.execute(cmd, stdout=False, sudo=True)
+
+            self.log('User %s deleted' % user)
+
+    def ensure_vhost(self, vhost):
+        '''
+        Ensure the given vhost is created.
+
+        :param vhost: Name of the vhost to be checked/created.
+        :type vhost: :class:`str`
+
+        :return: Whether the vhost had to be created or not.
+        :rtype: :class:`bool`
+
+        Example:
+        ::
+
+            class MySampleRole(Role):
+                def provision(self):
+                    with self.using(RabbitMqRole) as role:
+                        role.ensure_vhost('/some_vhost')
+        '''
+        if not self.vhost_exists(vhost):
+            self.log('Adding vhost %s' % vhost)
+
+            self.execute('rabbitmqctl add_vhost %s' % vhost, sudo=True)
+
+            self.log('vhost %s added!' % vhost)
+            return True
+        return False
+
+    def ensure_permission(self, vhost, username, perms):
+        '''
+        Ensure the given user has the given permissions on the specified vhost
+
+        :param vhost: Virtual host name to assign the permissions at.
+        :type vhost: :class:`str`
+        :param username: User to assign permissions to.
+        :type username: :class:`str`
+        :param perms: Permissions to assign to user (e.g.: '".*" ".*" ".*"').
+        :type perms: :class:`str`
+
+        :return: Whether the permissions could be assigned or not.
+        :rtype: :class:`bool`
+
+        Example:
+        ::
+
+            class MySampleRole(Role):
+                def provision(self):
+                    with self.using(RabbitMqRole) as role:
+                        role.ensure_permission(
+                            'previous_created_vhost',
+                            'previous_created_user',
+                            '".*" ".*" ".*"',
+                        )
+        '''
+        if not self.user_exists(username):
+            msg = 'Cannot set permission: User %s doesn\'t exist' % username
+            self.log(msg)
+            return False
+
+        if not self.vhost_exists(vhost):
+            self.log('Cannot set permission: vhost %s doesn\'t exist' % vhost)
+            return False
+
+        msg = 'Setting up permissions for user %s on vhost %s'
+        msg = msg % (username, vhost)
+        self.log(msg)
+
+        args = (vhost, username, perms)
+        cmd = 'rabbitmqctl set_permissions -p %s %s %s' % args
+        self.execute(cmd, stdout=False, sudo=True)
+
+        return True
---
+++ tests/unit/more/debian/messaging/test_rabbitmq.py
@@ -0,0 +1,208 @@
+from mock import call, patch
+from nose.tools import istest
+
+from provy.more.debian import RabbitMqRole, AptitudeRole
+from provy.more.debian.messaging.rabbitmq import GUEST_USER_WARNING
+from tests.unit.tools.helpers import ProvyTestCase
+
+
+class RabbitMqRoleTest(ProvyTestCase):
+    def setUp(self):
+        super(RabbitMqRoleTest, self).setUp()
+        self.role = RabbitMqRole(prov=None, context={})
+
+    def output_list_vhosts(self, vhosts):
+        all_strings = ['Listing vhosts ...'] + vhosts + ['...done.']
+        return '\r\n'.join(all_strings)
+
+    @istest
+    def checks_that_a_user_exists(self):
+        with self.execute_mock() as execute:
+            execute.return_value = ['john', 'jack']
+
+            result = self.role.user_exists('john')
+
+            self.assertTrue(result)
+            execute.assert_called_with('rabbitmqctl list_users', sudo=True, stdout=False)
+
+    @istest
+    def checks_that_a_user_doesnt_exist(self):
+        with self.execute_mock() as execute:
+            execute.return_value = ['john', 'jack']
+
+            result = self.role.user_exists('brian')
+
+            self.assertFalse(result)
+            execute.assert_called_with('rabbitmqctl list_users', sudo=True, stdout=False)
+
+    @istest
+    def installs_necessary_packages_to_provision(self, **mocks):
+        with self.using_stub(AptitudeRole) as mock_yum, self.mock_role_methods('is_process_running', 'user_exists', 'execute'):
+            self.role.is_process_running.return_value = True
+            self.role.user_exists.return_value = False
+
+            self.role.provision()
+
+            mock_yum.ensure_up_to_date.assert_called_with()
+            mock_yum.ensure_package_installed.assert_called_with('rabbitmq-server')
+
+    @istest
+    def executes_the_correct_commands_to_provision(self, **mocks):
+        with self.using_stub(AptitudeRole), self.mock_role_methods('is_process_running', 'user_exists', 'execute'):
+            self.role.is_process_running.return_value = False
+            self.role.user_exists.return_value = False
+
+            self.role.provision()
+
+            self.assertEqual(self.role.execute.mock_calls, [
+                call('update-rc.d rabbitmq-server defaults', stdout=False, sudo=True),
+                call('update-rc.d rabbitmq-server enable', stdout=False, sudo=True),
+                call('service rabbitmq-server start', stdout=False, sudo=True),
+            ])
+
+    @istest
+    def doesnt_start_rabbit_if_already_started_during_provisioning(self, **mocks):
+        with self.using_stub(AptitudeRole), self.mock_role_methods('is_process_running', 'user_exists', 'execute'):
+            self.role.is_process_running.return_value = True
+            self.role.user_exists.return_value = False
+
+            self.role.provision()
+
+            self.assertEqual(self.role.execute.mock_calls, [
+                call('update-rc.d rabbitmq-server defaults', stdout=False, sudo=True),
+                call('update-rc.d rabbitmq-server enable', stdout=False, sudo=True),
+            ])
+
+    @istest
+    def warns_about_guest_user(self, **mocks):
+        with self.using_stub(AptitudeRole), self.mock_role_methods('is_process_running', 'user_exists', 'execute'), patch('provy.more.centos.messaging.rabbitmq.warn') as warn:
+            self.role.is_process_running.return_value = True
+            self.role.user_exists.return_value = True
+
+            self.role.provision()
+
+            warn.assert_called_once_with(GUEST_USER_WARNING)
+
+    @istest
+    def checks_that_a_vhost_exists(self):
+        with self.execute_mock() as execute:
+            execute.return_value = self.output_list_vhosts([
+                'foo',
+                'bar',
+            ])
+
+            self.assertTrue(self.role.vhost_exists('foo'))
+            execute.assert_called_once_with('rabbitmqctl list_vhosts', stdout=False, sudo=True)
+
+    @istest
+    def checks_that_a_vhost_doesnt_exist(self):
+        with self.execute_mock() as execute:
+            execute.return_value = self.output_list_vhosts([
+                'foo',
+                'bar',
+            ])
+
+            self.assertFalse(self.role.vhost_exists('baz'))
+            execute.assert_called_once_with('rabbitmqctl list_vhosts', stdout=False, sudo=True)
+
+    @istest
+    def ensures_user_is_created_if_it_doesnt_exist_yet(self):
+        with self.mock_role_methods('user_exists', 'execute'):
+            self.role.user_exists.return_value = False
+
+            result = self.role.ensure_user('foo-user', 'foo-pass')
+
+            self.assertTrue(result)
+            self.role.user_exists.assert_called_once_with('foo-user')
+            self.role.execute.assert_called_once_with('rabbitmqctl add_user foo-user foo-pass', sudo=True)
+
+    @istest
+    def doesnt_create_user_if_it_already_exists(self):
+        with self.mock_role_methods('user_exists', 'execute'):
+            self.role.user_exists.return_value = True
+
+            result = self.role.ensure_user('foo-user', 'foo-pass')
+
+            self.assertFalse(result)
+            self.role.user_exists.assert_called_once_with('foo-user')
+            self.assertFalse(self.role.execute.called)
+
+    @istest
+    def deletes_user_if_it_exists(self):
+        with self.mock_role_methods('user_exists', 'execute'):
+            self.role.user_exists.return_value = True
+
+            self.role.delete_user('foo-user')
+
+            self.role.user_exists.assert_called_once_with('foo-user')
+            self.role.execute.assert_called_once_with('rabbitmqctl delete_user foo-user', sudo=True, stdout=False)
+
+    @istest
+    def doesnt_delete_user_if_it_doesnt_exist(self):
+        with self.mock_role_methods('user_exists', 'execute'):
+            self.role.user_exists.return_value = False
+
+            self.role.delete_user('foo-user')
+
+            self.role.user_exists.assert_called_once_with('foo-user')
+            self.assertFalse(self.role.execute.called)
+
+    @istest
+    def ensures_vhost_is_created_if_it_doesnt_exist_yet(self):
+        with self.mock_role_methods('vhost_exists', 'execute'):
+            self.role.vhost_exists.return_value = False
+
+            result = self.role.ensure_vhost('foo-vhost')
+
+            self.assertTrue(result)
+            self.role.vhost_exists.assert_called_once_with('foo-vhost')
+            self.role.execute.assert_called_once_with('rabbitmqctl add_vhost foo-vhost', sudo=True)
+
+    @istest
+    def doesnt_create_vhost_if_it_already_exists(self):
+        with self.mock_role_methods('vhost_exists', 'execute'):
+            self.role.vhost_exists.return_value = True
+
+            result = self.role.ensure_vhost('foo-vhost')
+
+            self.assertFalse(result)
+            self.role.vhost_exists.assert_called_once_with('foo-vhost')
+            self.assertFalse(self.role.execute.called)
+
+    @istest
+    def ensures_permission_is_given(self):
+        with self.mock_role_methods('user_exists', 'vhost_exists', 'execute'):
+            self.role.user_exists.return_value = True
+            self.role.vhost_exists.return_value = True
+
+            result = self.role.ensure_permission('foo-vhost', 'foo-user', 'foo bar')
+
+            self.assertTrue(result)
+            self.role.user_exists.assert_called_once_with('foo-user')
+            self.role.vhost_exists.assert_called_once_with('foo-vhost')
+            self.role.execute.assert_called_once_with('rabbitmqctl set_permissions -p foo-vhost foo-user foo bar', sudo=True, stdout=False)
+
+    @istest
+    def doesnt_give_permission_if_user_doesnt_exist(self):
+        with self.mock_role_methods('user_exists', 'vhost_exists', 'execute'):
+            self.role.user_exists.return_value = False
+            self.role.vhost_exists.return_value = True
+
+            result = self.role.ensure_permission('foo-vhost', 'foo-user', 'foo bar')
+
+            self.assertFalse(result)
+            self.role.user_exists.assert_called_once_with('foo-user')
+            self.assertFalse(self.role.execute.called)
+
+    @istest
+    def doesnt_give_permission_if_vhost_doesnt_exist(self):
+        with self.mock_role_methods('user_exists', 'vhost_exists', 'execute'):
+            self.role.user_exists.return_value = True
+            self.role.vhost_exists.return_value = False
+
+            result = self.role.ensure_permission('foo-vhost', 'foo-user', 'foo bar')
+
+            self.assertFalse(result)
+            self.role.user_exists.assert_called_once_with('foo-user')
+            self.role.vhost_exists.assert_called_once_with('foo-vhost')
+            self.assertFalse(self.role.execute.called)
