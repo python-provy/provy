eccab584d3b36296fb5b538745e0595d19b24c8c
Jacek Bzdak <jbzdak@gmail.com>
2014-01-02 18:55:15 +0100

    Added ability to specify return codes.

--- provy/core/errors.py
+++ provy/core/errors.py
@@ -1,6 +1,10 @@
-#!/usr/bin/python
 # -*- coding: utf-8 -*-
 
 
 class ConfigurationError(RuntimeError):
     '''Raised when there's a configuration error in the provyfile.'''
+
+class CommandExecutionError(RuntimeError):
+    '''
+    Raised when local command has invalid exitcode
+    '''
